369cfba48ea76eda8d2e12343597b6ede1ec7d1a
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-01-04 10:02:13 -0500

    New project's folder tree

---
+++ platforms/Explorer-16-PIC24/api/machine.h
@@ -0,0 +1,7 @@
+#ifndef _MACHINE_H_
+#define _MACHINE_H_
+
+#include "./machine/pin.h"
+#include "./machine/uart.h"
+
+#endif  //_MACHINE_H_ 
---
+++ platforms/Explorer-16-PIC24/api/time/sleep_us.h
@@ -0,0 +1,6 @@
+#ifndef _SLEEP_US_H_
+#define _SLEEP_US_H_
+
+#define sleep_us(TIME)    __delay_us(TIME)
+
+#endif  //_SLEEP_US_H_
