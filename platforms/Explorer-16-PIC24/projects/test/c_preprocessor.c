37e91b7b13811669cc2aab378836ae5d319fbef4
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-01-04 16:27:04 -0500

    C preprocessor support

---
+++ platforms/Explorer-16-PIC24/projects/test/c_preprocessor.c
@@ -0,0 +1,14 @@
+//Generated C file for:  Device = PIC24FJ128GA010  Board = Explorer 16
+
+#include "settings.h"
+
+//User defined headers files
+#include "time.h"
+
+int main(void) {
+
+	//AD1PCFG = 0xFFFF; // set all I/O digital
+	sleep(delay);
+	
+	return 0;
+}
