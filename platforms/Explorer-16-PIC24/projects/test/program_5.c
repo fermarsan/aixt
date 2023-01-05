1a9cbeff98a944e2e701d6292f27480eeeaa2f57
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-01-05 16:37:47 -0500

    all program type test files

---
+++ platforms/Explorer-16-PIC24/projects/test/program_5.c
@@ -0,0 +1,17 @@
+//Generated C file for:  Device = PIC24FJ128GA010  Board = Explorer 16
+
+#include "settings.h"
+
+
+// module;
+
+
+const float pi = 3.141592;
+const float e = 2.71828;
+
+
+int main(void) {
+//AD1PCFG = 0xFFFF; // set all I/O digital
+
+	return 0;
+}
