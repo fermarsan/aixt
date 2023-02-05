1e4a4bb46bc4259add3caa4c0eb8ed92152239bd
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-02-05 01:42:52 -0500

    vscode tasks: Transpile, compile and Biuld (NXC)

--- platforms/NXC/projects/test/const.v
+++ platforms/NXC/projects/test/const.v
@@ -1,6 +1,6 @@
 const (
-	t_on  = 1000
-	t_off = 500
+	t_ON  = 1000
+	t_OFF = 500
 )
 
 // const t_on = 1000
@@ -8,12 +8,12 @@ const (
 // const a = 34.67
 
 on_fwd(out_ab)
-wait(t_on)
+wait(t_ON)
 
 off(out_ab)
-wait(t_off)
+wait(t_OFF)
 
 on_fwd(out_ab)
-wait(t_on)
+wait(t_ON)
 
 off(out_ab)
