0eaba60dbf11f305ee75e7bcbdf33a0050dd1402
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-03-01 14:54:58 -0500

    pin: opt level 0

--- platforms/Explorer-16-PIC24/api/machine/machine__pin_0.c
+++ platforms/Explorer-16-PIC24/api/machine/machine__pin_0.c
@@ -4,28 +4,28 @@
 // 
 // Copyright (c) 2023 Fernando Martínez Santa
 
-void pin(PIN,TYPE) { 
+void pin(int PIN, int TYPE) { 
     addr = (int)(PIN/16)*3 + &TRISA;
     port_bit = PIN%16;
-    if(TYPE==IN) *addr |=   0x0001<<port_bit;
+    if(TYPE==PIN_IN) *addr |=   0x0001<<port_bit;
     else         *addr &= ~(0x0001<<port_bit);
 }
 
-void pin_high(PIN) {   
+void pin_high(int PIN) {   
     *((int)(PIN/16)*3 + &LATA)  |=   0x0001<<(PIN%16);
 }
 
-void pin_low(PIN) {    
+void pin_low(int PIN) {    
     *((int)(PIN/16)*3 + &LATA)  &= ~(0x0001<<(PIN%16));
 }
 
-void pin_write(PIN,VAL) {  
+void pin_write(int PIN, int VAL) {  
     addr = (int)(PIN/16)*3 + &LATA;
-    port_bit = PIN%16;
+    port_bit = PIN % 16;
     *addr &= (~((0x0001)<<port_bit));
     *addr |= (VAL<<port_bit);  
 }
 
-void pin_read(PIN) {   
+void pin_read(int PIN) {   
     ((*((int)(PIN/16)*3 + &PORTA) >> (PIN%16)) & 0x0001);
 }
