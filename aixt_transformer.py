eb961c791ed53bfcd7adca4ac97ed64cf5b160e5
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-04-03 23:51:18 -0500

    output format fixed

--- aixt_transformer.py
+++ aixt_transformer.py
@@ -7,7 +7,6 @@
 from lark import Transformer, v_args
 from lark.lexer import Token
 import yaml
-import re
 
 @v_args(inline=True)
 class aixtTransformer(Transformer):
@@ -43,14 +42,13 @@ class aixtTransformer(Transformer):
         with open(name,'w') as outText:
             s = '// NXC ' if self.setup['nxc'] else '// C '
             s += 'code generated from the Aixt source'
-            s += '\n// Device = ' + self.setup['device'] 
+            s += '\n// Device = ' + self.setup['device']
             s += '\n// Board = ' + self.setup['board'] + '\n'
             s += '#include "settings.h"\n\n' if not self.setup['nxc'] else '\n'
             # s += '// ' + self.moduleDef + '\n'  #module definition
             # s += self.includes + '\n'            #user defined headers files
             for td in self.topDecl:
-                s += '{};\n'.format(td) #top level declarations      
-            s += '\n'
+                s += '{}\n'.format(td) #top level declarations      
             if not self.main:       #adds the main function structure if not exist
                 s += 'task' if self.setup['nxc'] else ''
                 s += self.setup['main_ret_type'] if self.setup['main_ret_type'] != 'none' else ''
@@ -70,6 +68,7 @@ class aixtTransformer(Transformer):
     @v_args(inline=False)
     def source_file(self, sf):
         print('source_file:', sf)
+        print('source_file:', self.topDecl)
         s = ''
         for ds in sf:
             for st in ds:
@@ -282,7 +281,7 @@ class aixtTransformer(Transformer):
 
     @v_args(inline=False)
     def if_expr(self, ie):
-        s = 'if({}){}'.format(ie[1], ie[2]) # "if" + expr + block
+        s = 'if({}) {}'.format(ie[1], ie[2]) # "if" + expr + block
         n = len(ie)
         if n > 3:
             for i in range(3,n):
