02b2770583dbb4bc03a514ae3bad159e77a427a2
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-04-09 07:58:39 -0500

    Linux port basic folder tree

---
+++ platforms/Linux/api/builtin.h
@@ -0,0 +1,5 @@
+#define print(STR)      printf(STR)
+
+#define println(STR) \
+    printf(STR); \
+    printf("\n");
---
+++ platforms/Linux/projects/common_test/6_comparing_operators.v
@@ -0,0 +1,11 @@
+// all the comparing operators
+
+a, b := 10, 20
+l := false
+
+l = a == b
+l = a != b
+l = a <= b
+l = a >= b
+l = a < b 
+l = a > b 
