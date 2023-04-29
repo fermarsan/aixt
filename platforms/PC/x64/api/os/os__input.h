c9b42d70a8d744e2217d5f74edc8a74eabdd2838
fermarsan <fmartinezsanta@gmail.com>
2023-04-29 12:59:28 -0500

    Aixt header

--- platforms/PC/x64/api/os/os__input.h
+++ platforms/PC/x64/api/os/os__input.h
@@ -1,3 +1,9 @@
+// This file is part of the Aixt project, https://gitlab.com/fermarsan/aixt-project.git
+//
+// The MIT License (MIT)
+// 
+// Copyright (c) 2022-2023 Fernando Martínez Santa
+
 #ifndef _OS__INPUT_H_
 #define _OS__INPUT_H_
 
