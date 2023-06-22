732a0c2ff99d62aee1d8e7b59dc2c88603afac30
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-06-21 23:50:40 -0500

    V: builder

--- aixt_build/builder.v
+++ aixt_build/builder.v
@@ -11,22 +11,25 @@ import v.pref
 import v.parser
 import aix_cgen
 
-struct Builder {
-	pref 		&pref.Preferences = unsafe { nil }
-	table		ast.Table = unsafe { nil }
-mut:
-	out			string
-	file_name	string
-}
+pub fn build_file(path string) {	
+	mut my_gen := aixt_cgen.Gen{
+		file: &ast.File{}
+		pref: &pref.Preferences{}
+		table: ast.new_table()
+		out: ''
+	}
 
-fn cemb_build(path string) {
+	my_gen.pref.is_script = true
+	
+	println(my_gen.gen('tst2.v'))
+
 	// println(os.args)
 	// println(os.args.len)
 
 	if os.args.len > 1 {
 		device := os.args[2]
 		// path := os.args[1]
-		table := ast.new_table()	
+		table := ast.new_table()
 		mut vpref := &pref.Preferences{}
 		vpref.is_script = true
 
@@ -35,30 +38,32 @@ fn cemb_build(path string) {
 		mut checker_ := checker.new_checker(table, vpref)
 		checker_.check(tree)
 		// println(tree)
-		// println(strings.repeat_string('_', 60) + '\n')
+		// println('_'.repeat(60) + '\n')
 		// println(table)
-		println(strings.repeat_string('_', 60) + '\n')
+		println('_'.repeat(60) + '\n')
 
 		println(vpref.path)
 
 		// mut trans_code := c_embedded.gen(tree)
 		mut trans_code := ''
 		if os.args.len > 2 {
-			if os.args[2] == '-nxc' {	// if -nxt flag
-				equivalents := toml.parse_file('../api/equivalents.toml') or { panic('file does not exist. ') }
+			if os.args[2] == '-nxc' { // if -nxt flag
+				equivalents := toml.parse_file('../api/equivalents.toml') or {
+					panic('file does not exist. ')
+				}
 				for eq in equivalents.to_any().as_map().keys() {
-					trans_code = trans_code.replace(eq,equivalents.value(eq).string())	// replace the NXC equivalents
+					trans_code = trans_code.replace(eq, equivalents.value(eq).string()) // replace the NXC equivalents
 				}
 			} else {
 				println('Invalid flag.\n')
 			}
 		}
 		println(trans_code)
-		println(strings.repeat_string('_', 60) + '\n')
+		println('_'.repeat(60) + '\n')
 
 		// saves the output file
 		output_ext := if os.args.len > 2 && os.args[2] == '-nxc' { '.nxc' } else { '.c' }
-		output_path := path.replace('.v', output_ext)   
+		output_path := path.replace('.v', output_ext)
 		// os.write_file(output_path, trans_code)!
 	} else {
 		println('no input file path.\n')
@@ -66,10 +71,10 @@ fn cemb_build(path string) {
 }
 
 pub fn build_api(path string) {
-	file_list := os.walk_ext(path, '.aixt')		// transpile secondary files
-	for file in file_list { 
+	file_list := os.walk_ext(path, '.aixt') // transpile secondary files
+	for file in file_list {
 		// if file != input_name {
-			println('Building API........')// os.execute('v run ${aixt_builder} ${device} ${file}').output) 
+		println('Building API........') // os.execute('v run ${aixt_builder} ${device} ${file}').output)
 		// }
 	}
 }
--- aixt_cgen/cgen.v
+++ aixt_cgen/cgen.v
@@ -1,7 +1,6 @@
 // This file is part of the Aixt project, https://gitlab.com/fermarsan/aixt-project.git
-// Governed by an MIT license (MIT)
+// it is governed by an MIT license (MIT)
 // Copyright (c) 2023 Fernando Martínez Santa
-
 module aixt_cgen
 
 import v.ast
@@ -11,33 +10,33 @@ import v.parser
 import v.checker
 import v.ast.walker
 
-struct Gen {
-	pref        &pref.Preferences = unsafe { nil }
-	table       &ast.Table = unsafe { nil }
+pub struct Gen {
 mut:
-	out			string
-	file		&ast.File = unsafe { nil }
+	file &ast.File = unsafe { nil }
+pub mut:
+	pref  &pref.Preferences = unsafe { nil }
+	table &ast.Table        = unsafe { nil }
+	out   string
 }
 
-pub fn gen(file &ast.File, table &ast.Table, pref_ &pref.Preferences) string {
-	mut aixt_gen := AixtGen{
-		pref:		pref_
-		table:		table
-		out:		''
-		file:		file
-	}
-	walker.inspect(aixt_gen.file, unsafe { nil }, unsafe{ aixt_gen.visit_gen } )
-	return aixt_gen.out
+pub fn (mut gen Gen) gen(source_path string) string {
+	// gen.pref.is_script = true
+	gen.file = parser.parse_file(source_path, gen.table, .skip_comments, gen.pref)
+	mut checker_ := checker.new_checker(gen.table, gen.pref)
+	checker_.check(gen.file)
+	walker.inspect(gen.file, unsafe { nil }, unsafe { gen.visit_gen })
+	return gen.out
 }
 
-fn (mut gen AixtGen) visit_gen(node &ast.Node, data voidptr) bool {
+fn (mut gen Gen) visit_gen(node &ast.Node, data voidptr) bool {
+	// println('\nHere..............\n')
 	match node {
 		ast.Expr {
-			match node{
-				ast.IfExpr {	// basic shape of an "if" expression
+			match node {
+				ast.IfExpr { // basic shape of an "if" expression
 					mut out := 'if(__cond__){\n__stmt__\n}\n'
 					out += if node.has_else { 'else{\n__stmt__\n}\n' } else { '' }
-					gen.out = gen.out.replace_once('__stmt__\n', out) 
+					gen.out = gen.out.replace_once('__stmt__\n', out)
 				}
 				else {}
 			}
@@ -46,35 +45,39 @@ fn (mut gen AixtGen) visit_gen(node &ast.Node, data voidptr) bool {
 			match node {
 				ast.FnDecl {
 					if node.is_main {
-						attrs	 := ''
-						ret_type := if node.return_type == ast.void_type_idx { 'void' } else { 'int' }
-						params 	 := if node.params == [] { 'void' } else { 'int' }
+						attrs := ''
+						ret_type := if node.return_type == ast.void_type_idx {
+							'void'
+						} else {
+							'int'
+						}
+						params := if node.params == [] { 'void' } else { 'int' }
 						gen.out += '${attrs} ${ret_type} main(${params}) {\n${'__stmt__\n'.repeat(node.stmts.len)}}'
-						gen.out = if gen.out[0] == ` ` { gen.out[1..] } else { gen.out } 
+						gen.out = if gen.out[0] == ` ` { gen.out[1..] } else { gen.out }
 					} else {
 						// attrs	 := ''
 						// ret_type := if node.return_type == ast.void_type_idx { 'void' } else { 'int' }
 						// params 	 := if node.params == [] { 'void' } else { 'int' }
 						// gen.out += '${attrs} ${ret_type} main(${params}) {\n${gen.temp_block_stmts(node.stmts.len)}}'
-						// gen.out = if gen.out[0] == ` ` { gen.out[1..] } else { gen.out } 
+						// gen.out = if gen.out[0] == ` ` { gen.out[1..] } else { gen.out }
 					}
 				}
 				ast.AssignStmt {
 					mut assign := ''
 					for i in 0 .. node.left.len {
-						if node.op == token.Kind.decl_assign {	// in case of declaration 
-							var_type := ast.new_table().type_symbols[ node.right_types[i].idx() ] 
+						if node.op == token.Kind.decl_assign { // in case of declaration
+							var_type := ast.new_table().type_symbols[node.right_types[i].idx()]
 							assign += '${var_type} ${node.left[i]} = ${node.right[i]};\n'
-						} else {	// for the rest of assignments
+						} else { // for the rest of assignments
 							assign += '${node.left[i]} ${node.op} ${node.right[i]};\n'
 						}
 					}
-					gen.out = gen.out.replace_once('__stmt__\n', assign) 
+					gen.out = gen.out.replace_once('__stmt__\n', assign)
 				}
-				else{} 
+				else {}
 			}
-		} 
-		ast.IfBranch {	// statements block of "if" and "else" expressions
+		}
+		ast.IfBranch { // statements block of "if" and "else" expressions
 			gen.out = gen.out.replace_once('__cond__', '${node.cond}')
 			gen.out = gen.out.replace_once('__stmt__\n', '${'__stmt__\n'.repeat(node.stmts.len)}')
 		}
