26d11254ec224e2ecadf41361200217005c9a2a1
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-06-27 23:38:27 -0500

    Common test 06 & 07 checked

---
+++ common_tst/07_binary_operators.c
@@ -0,0 +1,24 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+void main(void) {
+long a = 2;
+long b = 1;
+long c = 0;
+c = a + b;
+c = a - b;
+c = a * b;
+c = a / b;
+c = a % b;
+c = a << b;
+c = a >> b;
+c = a | b;
+c = a & b;
+c = a ^ b;
+c = a || b;
+c = a && b;
+}
---
+++ common_tst/08_unary_operators.c
@@ -0,0 +1,12 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+void main(void) {
+long a = 0;
+long b = 5;
+a = __v.ast.PrefixExpr__;
+}
