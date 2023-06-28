596aabd5dd846dcbc3dccb67672928453dfc33da
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-06-27 23:27:50 -0500

    Common test 03 - 05 checked

---
+++ common_tst/05_assign_operators.c
@@ -0,0 +1,21 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+void main(void) {
+long a = 10;
+a = 2;
+a += 1;
+a -= 1;
+a *= 1;
+a /= 1;
+a &= 1;
+a |= 1;
+a ^= 1;
+a %= 1;
+a <<= 1;
+a >>= 1;
+}
