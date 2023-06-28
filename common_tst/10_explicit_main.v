2f56a92d193e94580027c16da156608b01c3c3f3
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-06-27 23:56:26 -0500

    Common test 09 & 10 checked

--- common_tst/10_explicit_main.v
+++ common_tst/10_explicit_main.v
@@ -1,4 +1,4 @@
 fn main() {
 	a, b, c := 10, 5, 0
-	c = a + b
+	c = a + b 
 }
---
+++ common_tst/11_arithmetic_and_logic.c
@@ -0,0 +1,27 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+void main(void) {
+short counter = 0;
+short delta = 1;
+counter = 10;
+counter = counter + delta / 2;
+counter = delta * 4 - 35.6;
+delta = counter % 3;
+delta = counter * __v.ast.ParExpr__;
+signed char state = 0;
+signed char condition = 0xAA;
+state = state & condition;
+state = !state | state;
+state = state ^ !condition;
+state = state << 6;
+state = __v.ast.ParExpr__ >> 2;
+bool logic1 = true;
+bool logic2 = false;
+logic1 = __v.ast.ParExpr__;
+logic2 = __v.ast.ParExpr__;
+}
