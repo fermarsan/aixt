0b44081bf8c3d3b605cf812c6f91aa66294e604b
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-07-08 08:06:57 -0500

    Common test 04 OK

---
+++ common_tst/04_underscore_literal.c
@@ -0,0 +1,13 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+int main() {
+	long value = 1256780;
+	long bin = 0b00111011;
+	long x = 0xA6F7;
+	return 0;
+}
