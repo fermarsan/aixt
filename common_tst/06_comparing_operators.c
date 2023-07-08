1cdd78cebd310846f39cf23fe139c249fb5c9f4d
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-07-08 08:13:24 -0500

    Common test 06 OK

---
+++ common_tst/06_comparing_operators.c
@@ -0,0 +1,19 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+int main() {
+	long a = 10;
+	long b = 20;
+	bool l = false;
+	l = a == b;
+	l = a != b;
+	l = a <= b;
+	l = a >= b;
+	l = a < b;
+	l = a > b;
+	return 0;
+}
