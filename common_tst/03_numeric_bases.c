07721d32a2ffda42c483fabdc0de1483ad6ecd59
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-07-08 08:05:56 -0500

    Common test 03 OK

---
+++ common_tst/03_numeric_bases.c
@@ -0,0 +1,14 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+int main() {
+	long a = 10;
+	long b = 0x34;
+	long c = 38;
+	long d = 0b00110101;
+	return 0;
+}
