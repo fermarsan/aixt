b0058821af3aa702661755cb1d901cb36c26b915
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-07-08 08:15:47 -0500

    Common test 07  OK

---
+++ common_tst/07_binary_operators.c
@@ -0,0 +1,25 @@
+// Aixt project (C generated code)
+// Device = x64
+// Board = PC
+
+#include <stdbool.h>
+
+
+int main() {
+	long a = 2;
+	long b = 1;
+	long c = 0;
+	c = a + b;
+	c = a - b;
+	c = a * b;
+	c = a / b;
+	c = a % b;
+	c = a << b;
+	c = a >> b;
+	c = a | b;
+	c = a & b;
+	c = a ^ b;
+	c = a || b;
+	c = a && b;
+	return 0;
+}
