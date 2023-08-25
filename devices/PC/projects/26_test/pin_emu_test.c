a97e095a4b68a1a1641bea4f42a36637ad224d94
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-08-25 18:01:33 -0500

    time functions in PC port

--- devices/PC/projects/26_test/pin_emu_test.c
+++ devices/PC/projects/26_test/pin_emu_test.c
@@ -5,17 +5,17 @@
 // License: MIT
 //
 // Description: Pin emulation testing.
-#include <unistd.h>
-#include "machine__pin_emu.c"
+#include "../../api/time.c"
+#include "../../api/machine/machine__pin_emu.c"
 
 int main() {
     pin_update();
-    usleep(1500000);
+    sleep_ms(1500);
     pin_high(A);
     pin_write(B, 1);
-    usleep(1500000);
+    sleep_us(1500000);
     pin_low(A);
-    usleep(1500000);
+    sleep(2);
     pin_write(B, 0);
     printf("z = %i\n", pin_read(z));
     return 0;
