0e0ef83bf5ec9a53e3fa9702d9d279488663908e
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-09-03 22:08:46 -0500

    variable strings comparison

--- common_test/26_strings.v
+++ common_test/26_strings.v
@@ -1,9 +1,11 @@
 var := ''
 var2 := ''
 str := 'constant'
+b1 := false
 
 var = str
 var += ' '
 var += str
 
 var2 = var + str
+b1 = var == var2
--- ports/PC/api/string.c
+++ ports/PC/api/string.c
@@ -14,6 +14,8 @@ char __temp_str[30];
 
 #define __string_append(DEST, SOURCE)   strcat(DEST, SOURCE)  
 
+#define __string_comp(STR1, STR2)       strcmp(STR1, STR2)
+
 char *__string_add(char *str1, char *str2) {
     strcpy(__temp_str, str1);
     strcat(__temp_str, str2);
