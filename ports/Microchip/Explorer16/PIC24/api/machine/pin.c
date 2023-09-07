9cf403460c88ecd3b4f24222ec0897a4de46b993
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-09-07 00:44:26 -0500

    module variable names start with "__"

--- ports/Microchip/Explorer16/PIC24/api/machine/pin.c
+++ ports/Microchip/Explorer16/PIC24/api/machine/pin.c
@@ -8,17 +8,17 @@
 //              (Explorer16-PIC24 port)
 #include <p24FJ128GA010.h>
 
+#define out 0   // pin direction
 #define in  1
-#define out 0
 
-int *addr;      //PORT address pointer
-int port_bit;   //specific bit of PORT
+int *__addr;          //PORT address pointer
+int __port_bit;     //specific bit of PORT
 
 #define pin_mode(PIN,TYPE)   \
-    addr = (int)(PIN/16)*3 + &TRISA; \
-    port_bit = PIN%16; \
-    if(TYPE==in) *addr |=   0x0001<<port_bit; \
-    else         *addr &= ~(0x0001<<port_bit)
+    __addr = (int)(PIN/16)*3 + &TRISA; \
+    __port_bit = PIN%16; \
+    if(TYPE==in) *__addr |=   0x0001<<__port_bit; \
+    else         *__addr &= ~(0x0001<<__port_bit)
 
 #define pin_high(PIN)    \
     *((int)(PIN/16)*3 + &LATA)  |=   0x0001<<(PIN%16)
@@ -27,20 +27,20 @@ int port_bit;   //specific bit of PORT
     *((int)(PIN/16)*3 + &LATA)  &= ~(0x0001<<(PIN%16))
 
 #define pin_write(PIN,VAL)   \
-    addr = (int)(PIN/16)*3 + &LATA; \
-    port_bit = PIN%16; \
-    *addr &= (~((0x0001)<<port_bit)); \
-    *addr |= (VAL<<port_bit)  
+    __addr = (int)(PIN/16)*3 + &LATA; \
+    __port_bit = PIN%16; \
+    *__addr &= (~((0x0001)<<__port_bit)); \
+    *__addr |= (VAL<<__port_bit)  
 
 #define pin_read(PIN)    \
     ((*((int)(PIN/16)*3 + &PORTA) >> (PIN%16)) & 0x0001)
 
 
 // void pin_mode(int PIN, int TYPE) { 
-//     addr = (int)(PIN/16)*3 + &TRISA;
-//     port_bit = PIN%16;
-//     if(TYPE==in) *addr |=   0x0001<<port_bit;
-//     else         *addr &= ~(0x0001<<port_bit);
+//     __addr = (int)(PIN/16)*3 + &TRISA;
+//     __port_bit = PIN%16;
+//     if(TYPE==in) *__addr |=   0x0001<<__port_bit;
+//     else         *__addr &= ~(0x0001<<__port_bit);
 // }
 
 // void pin_high(int PIN) {   
@@ -52,10 +52,10 @@ int port_bit;   //specific bit of PORT
 // }
 
 // void pin_write(int PIN, int VAL) {  
-//     addr = (int)(PIN/16)*3 + &LATA;
-//     port_bit = PIN % 16;
-//     *addr &= (~((0x0001)<<port_bit));
-//     *addr |= (VAL<<port_bit);  
+//     __addr = (int)(PIN/16)*3 + &LATA;
+//     __port_bit = PIN % 16;
+//     *__addr &= (~((0x0001)<<__port_bit));
+//     *__addr |= (VAL<<__port_bit);  
 // }
 
 // void pin_read(int PIN) {   
