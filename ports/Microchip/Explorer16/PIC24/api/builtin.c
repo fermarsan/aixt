e987d1228c00210107a9d7ae4efabeb7f809c995
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-09-29 12:56:41 -0500

    adds "r" to the pin names

--- ports/Microchip/Explorer16/PIC24/api/builtin.c
+++ ports/Microchip/Explorer16/PIC24/api/builtin.c
@@ -6,28 +6,28 @@
 //
 // Description: Builtin definitions
 //              (Explorer16-PIC24)
-#define led3    a0  // Onboard LED            
-#define led4    a1        
-#define led5    a2        
-#define led6    a3       
-#define led7    a4       
-#define led8    a5        
-#define led9    a6        
-#define led10   a7    
+#define led3    ra0  // Onboard LED            
+#define led4    ra1        
+#define led5    ra2        
+#define led6    ra3       
+#define led7    ra4       
+#define led8    ra5        
+#define led9    ra6        
+#define led10   ra7    
 
-#define sw3     d6  // Onboard switchs  
-#define sw4     d13  
-#define sw5     a7  
-#define sw6     d7  
+#define sw3     rd6  // Onboard switchs  
+#define sw4     rd13  
+#define sw5     ra7  
+#define sw6     rd7  
 
-#define pot     b5  // Onboard potentiometer
+#define pot     rb5  // Onboard potentiometer
 
 enum __pin_names {
-    a0 = 0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, //port A  
-        b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, //port B
-        c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, //port C
-        d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, //port D
-        e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, //port E
-        f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, //port F
-        g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15  //port G
+    ra0, ra1, ra2, ra3, ra4, ra5, ra6, ra7, ra8, ra9, ra10, ra11, ra12, ra13, ra14, ra15, //port A  
+    rb0, rb1, rb2, rb3, rb4, rb5, rb6, rb7, rb8, rb9, rb10, rb11, rb12, rb13, rb14, rb15, //port B
+    rc0, rc1, rc2, rc3, rc4, rc5, rc6, rc7, rc8, rc9, rc10, rc11, rc12, rc13, rc14, rc15, //port C
+    rd0, rd1, rd2, rd3, rd4, rd5, rd6, rd7, rd8, rd9, rd10, rd11, rd12, rd13, rd14, rd15, //port D
+    re0, re1, re2, re3, re4, re5, re6, re7, re8, re9, re10, re11, re12, re13, re14, re15, //port E
+    rf0, rf1, rf2, rf3, rf4, rf5, rf6, rf7, rf8, rf9, rf10, rf11, rf12, rf13, rf14, rf15, //port F
+    rg0, rg1, rg2, rg3, rg4, rg5, rg6, rg7, rg8, rg9, rg10, rg11, rg12, rg13, rg14, rg15  //port G
 };  
