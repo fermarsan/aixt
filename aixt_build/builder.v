a512c4e7d54646dd94f41dd43a35d23196bbc988
Fernando Martínez Santa <fmartinezsanta@gmail.com>
2023-10-01 02:15:00 -0500

    .ino extension

--- aixt_build/builder.v
+++ aixt_build/builder.v
@@ -34,7 +34,11 @@ pub fn transpile_file(path string, setup_file toml.Doc, aixt_path string) {
 	transpiled := c_gen.gen(path) // transpile Aixt (V) to C
 
 	// saves the output file
-	output_ext := if c_gen.setup.value('backend').string() == 'nxc' { '.nxc' } else { '.c' }
+	output_ext := match c_gen.setup.value('backend').string() {
+		'nxc' 		{ '.nxc' }
+		'arduino'	{ '.ino' } 
+		else 		{ '.c' }
+	}
 	output_path := path.replace('.v', output_ext)
 	// println('\n${output_path}\n')
 	os.write_file(output_path, transpiled) or {}
