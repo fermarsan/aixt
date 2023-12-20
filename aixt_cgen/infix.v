ceb72f8354b8fa5c3627d9f005383de1b3e507c5
fermarsan <fmartinezsanta@gmail.com>
2023-12-19 22:15:14 -0500

    string functions inside

--- aixt_cgen/infix.v
+++ aixt_cgen/infix.v
@@ -24,7 +24,8 @@ fn (mut gen Gen) infix_expr(node ast.InfixExpr) string {
 			} 
 			'+' {
 				gen.add_include('string.h')
-				return '__string_add(${gen.ast_node(node.left)}, ${gen.ast_node(node.right)})'
+				gen.add_definition('char __temp_str[30];')
+				return 'strcat(strcpy(__temp_str, ${gen.ast_node(node.left)}), ${gen.ast_node(node.right)}))'
 			}	
 			else {
 				panic('\n\nTranspiler Error:\n"${node.op.str()}" operator not supported for strings.\n')
