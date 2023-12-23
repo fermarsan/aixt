dd1dff6ee78b40250f7c3a068c5eb2d56815d119
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2023-12-22 18:03:13 -0800

    fix: handle the case where less than 4 arguments are supplied (show help)

--- aixt.v
+++ aixt.v
@@ -13,54 +13,57 @@ import toml
 import aixt_build
 
 fn main() {
-	aixt_path := os.abs_path(os.dir(os.args[0])) // aixt base path
-	command := os.args[1] // command
-
-	if command in ['help', '--help', '-h'] {	// help message
+	if os.args.len < 4 {
 		println(help_message())
 	} else {
-		port, input_name := os.args[2], os.abs_path(os.args[3]) // the other parameters
-		mut base_name := input_name.replace('.aixt', '') // input file base name
-		base_name = base_name.replace('.v', '')
-		setup := toml.parse_file('${aixt_path}/ports/setup/${port}.toml') or { return } // load the device's setup file
-		match command {
-			'transpile', '-t' {
-				aixt_build.transpile_file(input_name, setup, aixt_path)
-				println('\n${input_name} transpilation finished.\n')
-			}
-			'compile', '-c' {
-				aixt_build.compile_file(base_name, setup)
-				ext := match setup.value('backend').string() {
-					'nxc' 		{ 'nxc' }
-					'arduino'	{ 'ino' } 
-					else 		{ 'c' }
+		aixt_path := os.abs_path(os.dir(os.args[0])) // aixt base path
+		command := os.args[1] // command
+		if command in ['help', '--help', '-h'] { // help message
+			println(help_message())
+		} else {
+			port, input_name := os.args[2], os.abs_path(os.args[3]) // the other parameters
+			mut base_name := input_name.replace('.aixt', '') // input file base name
+			base_name = base_name.replace('.v', '')
+			setup := toml.parse_file('${aixt_path}/ports/setup/${port}.toml') or { return } // load the device's setup file
+			match command {
+				'transpile', '-t' {
+					aixt_build.transpile_file(input_name, setup, aixt_path)
+					println('\n${input_name} transpilation finished.\n')
 				}
-				println('\n${base_name}.${ext} compilation finished.\n')
-			}
-			'build', '-b' {
-				aixt_build.transpile_file(input_name, setup, aixt_path)
-				println('\n${input_name} transpilation finished.\n')
-				aixt_build.compile_file(base_name, setup)
-				ext := match setup.value('backend').string() {
-					'nxc' 		{ 'nxc' }
-					'arduino'	{ 'ino' } 
-					else 		{ 'c' }
+				'compile', '-c' {
+					aixt_build.compile_file(base_name, setup)
+					ext := match setup.value('backend').string() {
+						'nxc' { 'nxc' }
+						'arduino' { 'ino' }
+						else { 'c' }
+					}
+					println('\n${base_name}.${ext} compilation finished.\n')
 				}
-				println('\n${base_name}.${ext} compilation finished.\n')
-			}
-			'clean', '-cl' {
-				os.rm('${base_name}.c') or {}	// clean c-type files
-				os.rm('${base_name}.nxc') or {}
-				os.rm('${base_name}.ino') or {}
-				$if windows {	// and executables
-					os.rm('${base_name}.exe') or {}	
-				} $else {
-					os.rm('${base_name}') or {}
+				'build', '-b' {
+					aixt_build.transpile_file(input_name, setup, aixt_path)
+					println('\n${input_name} transpilation finished.\n')
+					aixt_build.compile_file(base_name, setup)
+					ext := match setup.value('backend').string() {
+						'nxc' { 'nxc' }
+						'arduino' { 'ino' }
+						else { 'c' }
+					}
+					println('\n${base_name}.${ext} compilation finished.\n')
 				}
-				println('Output files cleaned.')
-			}
-			else {
-				println('Invalid command.')
+				'clean', '-cl' {
+					os.rm('${base_name}.c') or {} // clean c-type files
+					os.rm('${base_name}.nxc') or {}
+					os.rm('${base_name}.ino') or {}
+					$if windows { // and executables
+						os.rm('${base_name}.exe') or {}
+					} $else {
+						os.rm('${base_name}') or {}
+					}
+					println('Output files cleaned.')
+				}
+				else {
+					println('Invalid command.')
+				}
 			}
 		}
 	}
@@ -73,21 +76,21 @@ Usage:
   aixt command device input_file_name
 
 Examples:
-  aixt transpile NXC hello.aixt	Transpile the file `hello.aixt` and output it as 
-  								`hello.nxc`.
-  aixt build NXC hello.v    	Transpile, compile and download `hello.v` to NXT 
-  								Intelligent brick.
-  aixt -t Arduino_Nano hello.v	Transpile the file `hello.v` and output it as 
-  								`hello.c` or `hello.ino` (depending on the Backend).
+  aixt transpile NXC hello.aixt	Transpile the file `hello.aixt` and output it as
+                                `hello.nxc`.
+  aixt build NXC hello.v        Transpile, compile and download `hello.v` to NXT
+                                Intelligent brick.
+  aixt -t Arduino_Nano hello.v	Transpile the file `hello.v` and output it as
+                                `hello.c` or `hello.ino` (depending on the Backend).
   aixt -c Emulator hello.aixt	Compile a previously transpiled `hello.c` (from
-  								`hello.aixt`) and output it as `hello` or `hello.exe`
+                                `hello.aixt`) and output it as `hello` or `hello.exe`
   aixt clean hello.aixt         Clean all the generated files `hello.*`
 
 aixt supports the following commands:
 
-  transpile, -t   				Transpile to C an Aixt source file.
-  compile, -c     				Compile the previosly generated C file.
-  build, -b       				Build (transpile and compile) an Aixt program.
-  clean, -cl      				Clean all the generated file (C and executables).
+  transpile, -t                 Transpile to C an Aixt source file.
+  compile, -c                   Compile the previosly generated C file.
+  build, -b                     Build (transpile and compile) an Aixt program.
+  clean, -cl                    Clean all the generated file (C and executables).
   help, --help, -h				Call this help'
 }
