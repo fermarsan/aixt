3f1cac0936bb6340035a587ded10eb876e598e75
roa53 <cesaralejandro53@hotmail.com>
2024-01-10 03:08:10 -0500

    Actualizacion del archivo builtin.c

--- ports/Microchip/PIC16F676/api/builtin.c
+++ ports/Microchip/PIC16F676/api/builtin.c
@@ -7,6 +7,15 @@
 // Description: Builtin definitions
 //              (PIC16F676)
 
+// Project Name: Aixt project, https://gitlab.com/fermarsan/aixt-project.git
+// File Name: builtin.c
+// Author: Cesar Alejandro Roa Acosta and Fernando Martínez Santa
+// Date: 2023
+// License: MIT
+//
+// Description: Builtin definitions
+//              (PIC16F676)
+
 #define an0    ra0  // Onboard LED            
 #define an1    ra1        
 #define an2    ra2              
@@ -16,11 +25,30 @@
 #define an6    rc2   
 #define an7    rc3  
 
-enum __pin_names {
-     ra0,  ra1,  ra2,  ra3,  ra4,  ra5, _ra6, _ra7,   //port A  
-    _rb0, _rb1, _rb2, _rb3, _rb4, _rb5, _rb6, _rb7,   //port B
-     rc0,  rc1,  rc2,  rc3,  rc4,  rc5, _rc6, _rc7,   //port C
-    _rd0, _rd1, _rd2, _rd3, _rd4, _rd5, _rd6, _rd7,   //port D
-    _re0, _re1, _re2, _re3, _re4, _re5, _re6, _re7    //port E
+#define a0_s    TRISAbits.TRISA0    // pin configuration pits
+#define a1_s    TRISAbits.TRISA1 
+#define a2_s    TRISAbits.TRISA2 
+#define a3_s    TRISAbits.TRISA3
+#define a4_s    TRISAbits.TRISA4 
+#define a5_s    TRISAbits.TRISA5 
 
-}; 
+#define c0_s    TRISCbits.TRISC0    // pin configuration pits
+#define c1_s    TRISCbits.TRISC1 
+#define c2_s    TRISCbits.TRISC2 
+#define c3_s    TRISCbits.TRISC3
+#define c4_s    TRISCbits.TRISC4 
+#define c5_s    TRISCbits.TRISC5 
+
+#define a0      PORTAbits.RA0       // pin input and output pits 
+#define a1      PORTAbits.RA1 
+#define a2      PORTAbits.RA2 
+#define a3      PORTAbits.RA3 
+#define a4      PORTAbits.RA4 
+#define a5      PORTAbits.RA5 
+
+#define c0      PORTCbits.RC0       
+#define c1      PORTCbits.RC1 
+#define c2      PORTCbits.RC2 
+#define c3      PORTCbits.RC3 
+#define c4      PORTCbits.RC4 
+#define c5      PORTCbits.RC5
