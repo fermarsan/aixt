539d44663021eb949b082b877e0d5f6e2201398a
fermarsan <fmartinezsanta@gmail.com>
2024-01-24 23:31:37 -0500

    start splitting assign.v

--- aixt_cgen/assign.v
+++ aixt_cgen/assign.v
@@ -14,43 +14,43 @@ fn (mut gen Gen) assign_stmt(node ast.AssignStmt) string {
 	mut var_type := ''
 	for i in 0 .. node.left.len {
 		// println('\n\nXXXX--  ${(node.left[i] as ast.Ident)}  --XXXX\n\n')
-		var_name := '${node.left[i].str()}'
-		// var_global_name := '${node.left[i].str()}'
-		// var_name := '${gen.current_fn}.${var_global_name}'
+		// var_name := '${node.left[i].str()}'
+		var_global_name := '${node.left[i].str()}'
+		var_name := '${gen.current_fn}.${var_global_name}'
 		
 		// println(var_name)
 		if node.op.str() == ':=' { // declaration-assignment
 			// // gen.idents << node.left[i] as  ast.Ident
-			// gen.idents[var_name] = struct { // add the new symbol
-			// 	kind: ast.IdentKind.variable
-			// }
-			// // println('XXXX--  ${node.right_types[i]}  --XXXX\n')
-			// gen.idents[var_name].typ = match node.right_types[i] {
-			// 	ast.int_literal_type_idx { 
-			// 		ast.int_type_idx 
-			// 	}
-			// 	ast.float_literal_type_idx { 
-			// 		ast.f32_type_idx 
-			// 	}
-			// 	else { 
-			// 		node.right_types[i] 
-			// 	}	
-			// }
-			// var_type = gen.table.type_kind(gen.idents[var_name].typ).str()
-			var_type = gen.table.type_kind(gen.file.scope.children[0].objects[var_name].typ)
+			gen.idents[var_name] = struct { // add the new symbol
+				kind: ast.IdentKind.variable
+			}
+			// println('XXXX--  ${node.right_types[i]}  --XXXX\n')
+			gen.idents[var_name].typ = match node.right_types[i] {
+				ast.int_literal_type_idx { 
+					ast.int_type_idx 
+				}
+				ast.float_literal_type_idx { 
+					ast.f32_type_idx 
+				}
+				else { 
+					node.right_types[i] 
+				}	
+			}
+			var_type = gen.table.type_kind(gen.idents[var_name].typ).str()
+			// var_type = gen.table.type_kind(gen.file.scope.children[0].objects[var_name].typ)
 			// println('XXXX--${var_type}--XXXX\n')
 			match var_type {
 				'array' {
-					// gen.idents[var_name].len = (node.right[i] as ast.ArrayInit).exprs.len // array len
-					// gen.idents[var_name].elem_type = (node.right[i] as ast.ArrayInit).elem_type // element type
+					gen.idents[var_name].len = (node.right[i] as ast.ArrayInit).exprs.len // array len
+					gen.idents[var_name].elem_type = (node.right[i] as ast.ArrayInit).elem_type // element type
 					var_type = gen.table.type_kind((node.right[i] as ast.ArrayInit).elem_type).str()
 					out += '${gen.setup.value(var_type).string()} ' // array's element type
 					out += '${gen.ast_node(node.left[i])}[${(node.right[i] as ast.ArrayInit).exprs.len}] = '
 					out += '${gen.ast_node(node.right[i])};\n'
 				}
 				'string' {
-					// gen.idents[var_name].len = (node.right[i] as ast.StringLiteral).val.len // string len
-					// gen.idents[var_name].elem_type = ast.rune_type_idx // element type
+					gen.idents[var_name].len = (node.right[i] as ast.StringLiteral).val.len // string len
+					gen.idents[var_name].elem_type = ast.rune_type_idx // element type
 					if gen.idents[var_name].len != 0 {	// Constant strings
 						if gen.setup.value('fixed_size_strings').bool() {
 							gen.idents[var_name].kind = ast.IdentKind.constant
