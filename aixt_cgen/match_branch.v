436ca9383ad972d7e75a94cdf20004a634ebb123
fermarsan <fmartinezsanta@gmail.com>
2024-01-25 11:03:40 -0500

    match.v splitting finished

--- aixt_cgen/match_branch.v
+++ aixt_cgen/match_branch.v
@@ -1,17 +1,13 @@
 // Project Name: Aixt project, https://github.com/fermarsan/aixt.git
-// File Name: match.v
+// File Name: match_branch.v
 // Author: Fernando Martínez Santa
 // Date: 2023-2024
 // License: MIT
 //
-// Description: code generation for 'match' expressions and branches.
+// Description: code generation for 'match' branches.
 module aixt_cgen
 
 import v.ast
-
-fn (mut gen Gen) match_expr(node ast.MatchExpr) string {
-	return ''
-}
 
 fn (mut gen Gen) match_branch(node ast.MatchBranch) string { // statements match branch expressions
 	return ''
