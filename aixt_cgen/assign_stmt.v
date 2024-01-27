1f58d54d5ab2359d09c643454e0675b4733fad0e
fermarsan <fmartinezsanta@gmail.com>
2024-01-27 10:45:48 -0500

    assignment statement finished (v symbol table)

--- aixt_cgen/assign_stmt.v
+++ aixt_cgen/assign_stmt.v
@@ -11,19 +11,15 @@ import v.ast
 
 fn (mut gen Gen) assign_stmt(node ast.AssignStmt) string {
 	mut out := ''
-	mut var_type := ''
+	mut var_kind := ''
 	for i in 0 .. node.left.len {
-		// println('\n\nXXXX--  ${(node.left[i] as ast.Ident)}  --XXXX\n\n')
-		// var_name := '${node.left[i].str()}'
-		var_global_name := '${node.left[i].str()}'
-		var_name := '${gen.cur_fn}.${var_global_name}'
-
+		var_name := '${node.left[i].str()}'
+		var_kind = gen.table.type_kind(node.left_types[i]).str()
 		if node.op.str() == ':=' { // declaration-assignment
-			var_type = gen.table.type_kind(node.left_types[i]).str()
-			match var_type {
+			match var_kind {
 				'array' {
-					var_type = gen.table.type_kind((node.right[i] as ast.ArrayInit).elem_type).str()
-					out += '${gen.setup.value(var_type).string()} ' // array's element type
+					var_kind = gen.table.type_kind((node.right[i] as ast.ArrayInit).elem_type).str()
+					out += '${gen.setup.value(var_kind).string()} ' // array's element type
 					out += '${gen.ast_node(node.left[i])}[${(node.right[i] as ast.ArrayInit).exprs.len}] = '
 					out += '${gen.ast_node(node.right[i])};\n'
 				}
@@ -41,7 +37,7 @@ fn (mut gen Gen) assign_stmt(node ast.AssignStmt) string {
 					}
 				}
 				else {
-					out += '${gen.setup.value(var_type).string()} ${gen.ast_node(node.left[i])} = '
+					out += '${gen.setup.value(var_kind).string()} ${gen.ast_node(node.left[i])} = '
 					out += if node.right[i].type_name() == 'v.ast.CastExpr' {
 						'${gen.ast_node((node.right[i] as ast.CastExpr).expr)};\n'
 					} else {
@@ -50,56 +46,38 @@ fn (mut gen Gen) assign_stmt(node ast.AssignStmt) string {
 				}
 			}
 		} else { // for the rest of assignments
-			// match node.left[i] {
-			// 	ast.Ident {	// if it is a simple variable
-			// 		if var_name in gen.idents || var_global_name in gen.idents {	// if previously defined
-			// 			var_type = if var_name in gen.idents {
-			// 				gen.table.type_kind(gen.idents[var_name].typ).str()
-			// 			} else if var_global_name in gen.idents {
-			// 				gen.table.type_kind(gen.idents[var_global_name].typ).str()
-			// 			} else {
-			// 				panic('\n\nTranspiler Error:\nUndefined variable "${node.left[i]}".\n')
-			// 			}
-			// 			match var_type {
-			// 				'array' {
-			// 					if gen.setup.value('fixed_size_arrays').bool() {
-			// 						panic('\n\nTranspiler Error:\nFor now dynamic-size arrays are not allowed.\n')
-			// 					}
-			// 				}
-			// 				'string' {
-			// 					// if gen.setup.value('fixed_size_strings').bool() {
-			// 					// 	panic('\n\nTranspiler Error:\nFor now dynamic-size strings are not allowed.\n')
-			// 					// }
-			// 					if gen.idents[var_name].kind == ast.IdentKind.variable {
-			// 						match node.op.str() {
-			// 							'=' {
-			// 								gen.add_include('string.h')
-			// 								out += 'strcpy(${gen.ast_node(node.left[i])}, ${gen.ast_node(node.right[i])});\n'
-			// 							} 
-			// 							'+=' {
-			// 								gen.add_include('string.h')
-			// 								out += 'strcat(${gen.ast_node(node.left[i])}, ${gen.ast_node(node.right[i])});\n'
-			// 							}	
-			// 							else {
-			// 								panic('\n\nTranspiler Error:\n"${node.op.str()}" operator not supported for strings.\n')
-			// 							}
-			// 						}
-			// 					} else {
-			// 						panic('\n\nTranspiler Error:\n"${var_global_name}" is a constant string.\n')
-			// 					}
-			// 				}
-			// 				else {
-			// 					out += '${gen.ast_node(node.left[i])} ${node.op} ${gen.ast_node(node.right[i])};\n'
-			// 				}
-			// 			} 
-			// 		} else {
-			// 			panic('\n\nTranspiler Error:\nUndefined variable "${node.left[i]}".\n')
-			// 		}
-			// 	} 
-			// 	else {
-			// 		out += '${gen.ast_node(node.left[i])} ${node.op} ${gen.ast_node(node.right[i])};\n'
-			// 	}
-			// }
+			match node.left[i] {
+				ast.Ident {	// if it is a simple variable
+					match var_kind {
+						'array' {
+							if gen.setup.value('fixed_size_arrays').bool() {
+								panic('\n\nTranspiler Error:\nFor now dynamic-size arrays are not allowed.\n')
+							}
+						}
+						'string' {
+							match node.op.str() {
+								'=' {
+									gen.add_include('string.h')
+									out += 'strcpy(${gen.ast_node(node.left[i])}, ${gen.ast_node(node.right[i])});\n'
+								} 
+								'+=' {
+									gen.add_include('string.h')
+									out += 'strcat(${gen.ast_node(node.left[i])}, ${gen.ast_node(node.right[i])});\n'
+								}	
+								else {
+									panic('\n\nTranspiler Error:\n"${node.op.str()}" operator not supported for strings.\n')
+								}
+							}
+						}
+						else {
+							out += '${gen.ast_node(node.left[i])} ${node.op} ${gen.ast_node(node.right[i])};\n'
+						}
+					} 
+				} 
+				else {
+					out += '${gen.ast_node(node.left[i])} ${node.op} ${gen.ast_node(node.right[i])};\n'
+				}
+			}
 		}
 	} 
 	return out
