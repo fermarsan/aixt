2ed0b1a1874f7b2559f7ba2fd18bc54717a6965b
Lpinto18 <lapinto0@misena.edu.co>
2024-01-27 08:29:38 -0500

    funciones pwm y uart para el PIC16F886

---
+++ ports/Microchip/PIC16F886/api/machine/pwm.c
@@ -0,0 +1,26 @@
+// Project Name: Aixt project, https://gitlab.com/fermarsan/aixt-project.git
+// File Name: pic.c
+// Author: Luis Alfredo Pinto Medina and Fernando Martínez Santa
+// Date: 2024
+// License: MIT
+//
+// Description: Pin management functions
+//              (PIC16F886 port)
+
+// Función para configurar el PWM
+#define pwm_setup(pin1, pin2) \
+    PR2 = 0x3E;                        /* Valor del período del PWM */ \
+    CCPR##pin1##L = 0;                 /* Inicializa el registro CCP en 0 dependiendo el CCP que configure el usuario*/ \
+    TRISCbits.TRISC##pin2 = 0;         /* Configura el Pin correspondiente como salida dependiendo el pin que configure el usuario */ \
+    T2CON = 0b00000011;                /* Configuración del Timer 2 */ \
+    CCP##pin1##CON = 0b00001100;       /* Configura el registro CCP en modo PWM dependiendo el CCP que configure el usuario*/ \
+    TMR2 = 0;                          /* Inicializa el Timer 2 en 0 */ \
+    T2CONbits.TMR2ON = 1;              /* Enciende el Timer 2 */
+
+// Función para el ciclo de trabajo
+#define pwm_write(duty, pin) \
+    { \
+        unsigned int pwm; \
+        pwm = ((duty - 0) * (60 - 0) / (256 - 0) + 0); /* Calculando el ciclo de trabajo */ \
+        CCPR##pin##L = (pwm >> 2); /* Alamacena el valor del PWM */ \   
+    }
