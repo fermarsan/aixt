16c2bce27d511d9f6c1c2c4223abc8f89054fe38
fermarsan <fmartinezsanta@gmail.com>
2024-03-06 14:16:18 -0500

    Time module updated

--- ports/Emulator/api/time/sleep.c.v
+++ ports/Emulator/api/time/sleep.c.v
@@ -5,4 +5,9 @@
 module time
 
 // sleep is a delay function in seconds for the Aixt PC port. 
-#define time__sleep(TMS)  start_time = clock();  printf("\nt = %lu\n", start_time); while (clock() < start_time + TMS*1000000)
+#if defined(__linux) || defined(linux)
+#define time__sleep(TS)		sleep(TS)
+#else
+#define time__sleep(TS)		Sleep(TMS*1000)
+#endif
+// #define time__sleep(TMS)  start_time = clock();  printf("\nt = %lu\n", start_time); while (clock() < start_time + TMS*1000000)
