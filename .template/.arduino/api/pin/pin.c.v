401a33547e727f2e8fba6c9bccc5b9258ec35ab2
fermarsan <fmartinezsanta@gmail.com>
2024-03-09 13:40:00 -0500

    template update

--- .template/.arduino/api/pin/pin.c.v
+++
@@ -1,7 +0,0 @@
-// Project Name: Aixt, https://github.com/fermarsan/aixt.git
-// Author: Fernando Martínez Santa
-// Date: 2023-2024
-// License: MIT
-//
-// Description: This is a module to emulate digital pines in console.
-module pin
--- .template/.arduino/api/pin/v.mod
+++
@@ -1,7 +0,0 @@
-Module {
-	name:	'pin'
-	description:	'Aixt pin management module (template for Arduino).'
-	version:	'0.0.1'
-	license:	'MIT'
-	dependencies:	[]
-}
