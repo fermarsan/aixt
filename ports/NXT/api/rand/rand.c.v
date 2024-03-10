1a17a109fbed3b408bf9bf562a99f49e4ffd48b2
fermarsan <fmartinezsanta@gmail.com>
2024-03-10 12:05:05 -0500

    New API

---
+++ ports/NXT/api/rand/rand.c.v
@@ -0,0 +1,14 @@
+module rand
+
+// cstdlib.c
+#define     abort(...)              abort(__VA_ARGS__)
+#define     srand(...)              srand(__VA_ARGS__)
+#define     rand(...)               rand(__VA_ARGS__)
+#define     random(...)             Random(__VA_ARGS__)
+#define     sys_random_number(...)  SysRandomNumber(__VA_ARGS__)
+#define     sys_random_ex(...)      SysRandomEx(__VA_ARGS__)
+
+
+fn init() {
+
+}
--- ports/NXT/projects/NXC_book_examples/motors2.v
+++ ports/NXT/projects/NXC_book_examples/motors2.v
@@ -1,18 +1,19 @@
 import motor
+import sound
 
-motor.forward_reg(ac,50,regmode_idle)
+motor.forward_reg(ac, 50, regmode_idle)
 
 sleep_ms(2000)
 
 motor.off(ac)
-play_tone(4000,50)
+sound.play_tone(4000, 50)
 sleep_ms(1000)
 motor.reset_tacho_count(ac)
-motor.forward_reg(ac, 50, regmode_speed)
+motor.forward_reg(ac,  50,  regmode_speed)
 sleep_ms(2000)
 motor.off(ac)
-play_tone(4000,50)
+sound.play_tone(4000, 50)
 sleep_ms(1000)
-motor.forward_reg(ac, 50, regmode_sync)
+motor.forward_reg(ac,  50,  regmode_sync)
 sleep_ms(2000)
 motor.off(ac)
