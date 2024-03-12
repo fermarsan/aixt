dbbc7d15324378ff5c40ff9a2d6566e0fc468d6d
JCGUZMANZ <157906856+JCGUZMANZ@users.noreply.github.com>
2024-03-12 08:43:53 -0500

    Update uart.c.v

--- ports/Blue Pill/STM/api/uart/uart.c.v
+++ ports/Blue Pill/STM/api/uart/uart.c.v
@@ -4,7 +4,7 @@
 // Date: 2022-2024
 // License: MIT
 //
-// // Description: UART functions (Blue Pill_STM32F103C)
+// // Description: UART functions (STM32F103C)
 //              (PC port) 
 module uart
 
