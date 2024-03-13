291ca95165841aa7a91e6afd7e1b3f46e1cb2718
Lpinto18 <lapinto0@misena.edu.co>
2024-03-13 09:59:11 -0500

    actualizaciones archivos .C.V y toml

---
+++ ports/Microchip/PIC16F886/api/time/time.c.v
@@ -0,0 +1,14 @@
+// Project Name: Aixt project, https://gitlab.com/fermarsan/aixt-project.git
+// File Name: time.c.v
+// Author: Luis Alfredo Pinto Medina and Fernando Martínez Santa
+// Date: 2024
+// License: MIT
+//
+// Description: seconds delay function
+//              (PIC16F886 port)
+
+module time
+
+fn init() {
+
+}
