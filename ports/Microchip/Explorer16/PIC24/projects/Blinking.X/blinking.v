c9761bc7c75460541b418321afe56464a086474a
profesanta <profe.santa.mail@gmail.com>
2024-03-17 00:00:43 -0500

    `module.dent` issue solved

--- ports/Microchip/Explorer16/PIC24/projects/Blinking.X/blinking.v
+++ ports/Microchip/Explorer16/PIC24/projects/Blinking.X/blinking.v
@@ -1,11 +1,12 @@
 import time { sleep_ms }
 import pin
 
+const t1 = 500
 pin.setup(a0, pin.output)
 
 for {
 	pin.high(a0)
-	sleep_ms(500)
+	sleep_ms(t1)
 	pin.low(a0)
-	sleep_ms(500)
+	sleep_ms(t1)
 }
