c1bfaef9884ae9401c3920a46b4e0156d2dfc422
JCGUZMANZ <jcguzmanz@udistrital.edu.co>
2024-03-27 14:57:41 -0500

    ST

--- ports/ST/Blue-Pill/STM/api/builtin.c.v
+++ ports/ST/Blue-Pill/STM/api/builtin.c.v
@@ -12,7 +12,30 @@ enum __pin_names {    // enumerated type for the pin names
  
     RX=PA11
     TX=PA10
-
+    PA0
+    PA1
+    PA2
+    PA3
+    PA4
+    PA5
+    PA6
+    PA7
+    PB0
+    PB1
+    PB10
+    PB11
+    PB12
+    PB13
+    PB14
+    PB15
+    PA8
+    PB3
+    PB4
+    PB5
+    PB6
+    PB7
+    PB8
+    PB9
 
 }
 
--- ports/ST/Blue-Pill/STM/api/pin/write.c.v
+++ ports/ST/Blue-Pill/STM/api/pin/write.c.v
@@ -1,11 +1,3 @@
-// Project Name: Aixt project https://gitlab.com/fermarsan/aixt-project.git
-// File Name: write.c.v
-// Author: Fernando Martínez Santa - Julián Camilo Guzmán Zambrano - Juan Pablo Gonzalez Penagos
-// Date: 2022-2024
-// License: MIT
-//
-// // Description: WRITE functions (Blue Pill_STM32F103C)
-//              (PC port) 
 
 module pin
 
