4f52147f0da416f2efcee346894e62d70f3d8cd8
Fabizz2024 <ing.farojas@gmail.com>
2024-03-31 23:03:29 -0500

    API, archivo de Configuración .toml para Arduino-Nano y LQFP32-MiniEVB

---
+++ ports/Atmel/Arduino-Nano/api/uart/print.c.v
@@ -0,0 +1,8 @@
+// Project Name: Aixt, https://github.com/fermarsan/aixt.git
+// Author: Fabián A. Rojas Acosta, Alberto Pinzón Valero and Fernando Martínez Santa
+// Date: 2024
+// License: MIT
+module uart
+
+// Prints data to the serial port as human-readable ASCII text
+#define uart__print(MESSAGE)  Serial.print(MESSAGE)
---
+++ ports/LogicGreen/LQFP32-MiniEVB/api/pin/v.mod
@@ -0,0 +1,7 @@
+Module {
+	name:	'pin'
+	description:	'Aixt pin management module for LQFP32 MiniEVB Nano - LGT8F328P'
+	version:	'0.0.1'
+	license:	'MIT'
+	dependencies:	[]
+}
