3e0ed99464234f6d1fcc0fffc041656255b3aceb
camilo gutierrez <andcagual@gmail.com>
2024-04-01 20:37:18 -0500

    Blinking update

--- ports/Raspberry-Pi/Raspberry-Pi-Pico/projects/BLINKING/blinking/blinking.ino
+++ ports/Raspberry-Pi/Raspberry-Pi-Pico/projects/BLINKING/blinking/blinking.ino
@@ -20,39 +20,6 @@ void time__init();
 
 void pin__init();
 
-enum main____pin_names {
-	gp0,
-	gp1,
-	gp2,
-	gp3,
-	gp4,
-	gp5,
-	gp6,
-	gp7,
-	gp8,
-	gp9,
-	gp10,
-	gp11,
-	gp12,
-	gp13,
-	gp14,
-	gp15,
-	gp16,
-	gp17,
-	gp18,
-	gp19,
-	gp20,
-	gp21,
-	gp22,
-	gp23,
-	gp24,
-	gp25,
-	gp26,
-	gp27,
-	gp28,
-	gp29,
-};
-
 void main__init() {
 	time__init();
 	pin__init();
