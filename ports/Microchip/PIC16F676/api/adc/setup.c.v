19ea7fef7d9ba4c9d3de2cb274221d6ed309ead1
fermarsan <fmartinezsanta@gmail.com>
2024-04-21 15:35:27 -0500

    16f866 api update

--- ports/Microchip/PIC16F676/api/adc/setup.c.v
+++ ports/Microchip/PIC16F676/api/adc/setup.c.v
@@ -1,24 +1,10 @@
-// Project Name: Aixt project, https://gitlab.com/fermarsan/aixt-project.git
-// File Name: setup.c.v
+// Project Name: Aixt project, https://github.com/fermarsan/aixt.git
 // Author: Cesar Alejandro Roa Acosta and Fernando Martínez Santa
 // Date: 2024
 // License: MIT
 //
 // Description: ADC management functions
 //              (PIC16F676 port)
-
 module adc
 
-#define adc__setup()  ANSEL = 0b00000010;  ADCON0 = 0b10000000;  ADCON1 = 0b00110000;  ADCON0bits.ADON = 1     
-
-
-
-
-
-/*
-#define adc__setup() \
-    ANSEL = 0b00000100  /* Configura el pin AN1 como entrada analógica */ \
-    ADCON0 = 0b10000000 /* Habilita el ADC y selecciona AN1 como entrada */ \
-    ADCON1 = 0b00110000 /* Ajusta la referencia de voltaje */\
-    ADCON0bits.ADON = 1 /* Enciende en conversor ADC */
-*/
+fn C.setup()
--- ports/Microchip/PIC16F676/api/time/sleep_ms.c.v
+++ ports/Microchip/PIC16F676/api/time/sleep_ms.c.v
@@ -1,12 +1,10 @@
-// Project Name: Aixt project, https://gitlab.com/fermarsan/aixt-project.git
-// File Name: sleep_ms.c.v
+// Project Name: Aixt project, https://github.com/fermarsan/aixt.git
 // Author: Cesar Alejandro Roa Acosta and Fernando Martínez Santa
 // Date: 2024
 // License: MIT
 //
 // Description: Milliseconds delay function
 //              (PIC16F676 port)
-
 module time
 
 #define time__sleep_ms(TIME)    __delay_ms(TIME)  // implementing by a macro for saving memory
