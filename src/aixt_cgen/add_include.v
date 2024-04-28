a5b9b83b05edde67f8f57175a4af0f32d65e86ac
fermarsan <fmartinezsanta@gmail.com>
2024-04-28 00:07:29 -0500

    duplicated `#include` issue solved

--- src/aixt_cgen/add_include.v
+++ src/aixt_cgen/add_include.v
@@ -6,7 +6,7 @@ module aixt_cgen
 
 // add_include adds an include clause to the output file.
 fn (mut gen Gen) add_include(name_plus_ext string) {
-	println('gen.c_preproc_cmds = ${gen.c_preproc_cmds}')
+	// println('gen.c_preproc_cmds = ${gen.c_preproc_cmds}')
 	if '#include <${name_plus_ext}>\n' !in gen.c_preproc_cmds {
 		gen.c_preproc_cmds << '#include <${name_plus_ext}>\n'
 	}
