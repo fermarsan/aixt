b0deec553fcb4724bface2d311ad1ea92f242442
fermarsan <fmartinezsanta@gmail.com>
2024-05-17 17:58:36 -0500

    update

--- ports/Microchip/PIC16F886/api/adc/adc.c.v
+++ ports/Microchip/PIC16F886/api/adc/adc.c.v
@@ -6,52 +6,33 @@
 // Description: ADC management functions (PIC16F886 port)
 module adc
 
-#define	an0 	0b1111111111111110		// analog pins
-#define	an1 	0b1111111111111101			
-#define	an2 	0b1111111111111011			
-#define	an3 	0b1111111111110111			
-#define	an4 	0b1111111111101111			
-#define	an5 	0b1111111111011111			
-#define	an6 	0b1111111110111111			
-#define	an7 	0b1111111101111111			
-#define	an8 	0b1111111011111111			
-#define	an9 	0b1111110111111111			
-#define	an10	0b1111101111111111		
-// #define	an11	0b1111011111111111		
-// #define	an12	0b1110111111111111		
-// #define	an13	0b1101111111111111		
-// #define	an14	0b1011111111111111		
-// #define	an15	0b0111111111111111
-
-enum analog_channels {
-	an_ch0 = 0			
-	an_ch1			
-	an_ch2			
-	an_ch3			
-	an_ch4			
-	an_ch5			
-	an_ch6			
-	an_ch7			
-	an_ch8			
-	an_ch9			
-	an_ch10		
-	// an_ch11		
-	// an_ch12		
-	// an_ch13		
-	// an_ch14		
-	// an_ch15
-}
+#define	adc__in0 	0b1111111111111110		// analog pins
+#define	adc__in1 	0b1111111111111101			
+#define	adc__in2 	0b1111111111111011			
+#define	adc__in3 	0b1111111111110111			
+#define	adc__in4 	0b1111111111101111			
+#define	adc__in5 	0b1111111111011111			
+#define	adc__in6 	0b1111111110111111			
+#define	adc__in7 	0b1111111101111111			
+#define	adc__in8 	0b1111111011111111			
+#define	adc__in9 	0b1111110111111111			
+#define	adc__in10	0b1111101111111111		
+// #define	adc__in11	0b1111011111111111		
+// #define	adc__in12	0b1110111111111111		
+// #define	adc__in13	0b1101111111111111		
+// #define	adc__in14	0b1011111111111111		
+// #define	adc__in15	0b0111111111111111
 
 // ADC frequency
-#define sr_30ksps_1mhz	0	// VREF 3.0v
-#define sr_30ksps_4mhz	1	// VREF 3.0v 
-#define sr_18ksps_8mhz	2	// VREF full range
-#define sr_21ksps_10mhz	2	// VREF full range
-#define sr_24ksps_12mhz	2	// VREF 3.0v
-#define sr_30ksps_16mhz	2	// VREF 3.0v
-#define sr_34ksps_20mhz	2	// VREF 3.0v
-#define sr_18Ksps_irc		3	// sleep mode FOSC > 1Mhz
+#define adc__30ksps_1mhz	0	// VREF 3.0v
+#define adc__30ksps_4mhz	1	// VREF 3.0v 
+#define adc__18ksps_8mhz	2	// VREF full range
+#define adc__21ksps_10mhz	2	// VREF full range
+#define adc__24ksps_12mhz	2	// VREF 3.0v
+#define adc__30ksps_16mhz	2	// VREF 3.0v
+#define adc__34ksps_20mhz	2	// VREF 3.0v
+#define adc__18Ksps_irc		3	// sleep mode FOSC > 1Mhz
 
 // ADC conversion bits
-#define	nbits_8		0
-#define	nbits_10	1
+#define	adc__bits_8		0
+#define	adc__bits_10	1
