0ede8c2b7ec0ef7dd0a581761448e6fd4880b68c
fermarsan <fmartinezsanta@gmail.com>
2024-05-19 19:12:59 -0500

    for-in-array indexing issue solved

--- src/aixt_cgen/for_in_stmt.v
+++ src/aixt_cgen/for_in_stmt.v
@@ -10,25 +10,28 @@ import v.ast
 fn (mut gen Gen) for_in_stmt(node ast.ForInStmt) []string {
 	mut out := []string{}
 	mut c_line := ''
-	if node.high.type_name() == 'v.ast.EmptyExpr' { // in an array
+	if node.kind.str() == 'array' {	// node.high.type_name() == 'v.ast.EmptyExpr' { // in an array
+	// println('=============== ${node} ===============')
 		gen.level_count++
-		temp_name := '_t${gen.level_count}'	// temp variables by levels
-		if !gen.files[gen.file_count].scope.known_var(temp_name) {	//if doesn't exits, create it
+		index_name := '__t${gen.level_count}'	// temporal variables (indexes) by levels
+		if !gen.files[gen.file_count].scope.known_var(index_name) {	//if doesn't exits, create it
 			mut temp_var := ast.Var {
-				name: temp_name
+				name: index_name
 				typ: ast.Type(ast.Kind.int)
 			}
-			gen.files[gen.file_count].scope.objects[temp_name] = temp_var //
-			gen.definitions << 'int ${temp_name};'
+			gen.files[gen.file_count].scope.objects[index_name] = temp_var //
+			gen.definitions << 'int ${index_name};'
 		}
 		var := *gen.files[gen.file_count].scope.children[0].find_var(node.cond.str()) or { }
-		c_line += 'for(int ${temp_name} = 0;'
-		c_line += ' ${temp_name} < ${(var.expr as ast.ArrayInit).exprs.len};'
-		out << c_line + ' ${temp_name}++) {'
+		c_line += 'for(int ${index_name} = 0;'
+		c_line += ' ${index_name} < ${(var.expr as ast.ArrayInit).exprs.len};'
+		out << c_line + ' ${index_name}++) {'
+		val_var_kind := gen.table.type_kind(node.val_type).str()
+		out << '${gen.setup.value(val_var_kind).string()} ${node.val_var};'	// declare the element variable
+		out << '${node.val_var} = ${gen.ast_node(node.cond).join('')}[${index_name}];'	// read the element by index
 		for st in node.stmts {
-			out << gen.ast_node(st).join('\n').replace(node.val_var, '${gen.ast_node(node.cond).join('')}[${temp_name}]')
+			out << gen.ast_node(st)
 		}
-		// out = out.replace(node.val_var, '${gen.ast_node(node.cond)}[${temp_name}]')
 	} else { // in a range
 		c_line += 'for(int ${node.val_var}=${gen.ast_node(node.cond).join('')}; '
 		out << c_line + '${node.val_var}<${gen.ast_node(node.high).join('')}; ${node.val_var}++) {'
