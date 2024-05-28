4fb3d5c39cc15ed9c9586045298e3f9ff1fe95d0
fermarsan <fmartinezsanta@gmail.com>
2024-05-27 20:35:49 -0500

    Multiple module import issue solved

--- ports/Microchip/Explorer16/PIC24/api/sw_uart/sw_uart.c.v
+++ ports/Microchip/Explorer16/PIC24/api/sw_uart/sw_uart.c.v
@@ -6,17 +6,17 @@ import pin
 #include	<stdlib.h>		// for itoa()
 
  // define serial pins tx and rx
-#define sw_uart___tx	b, 0	// Tx : PORTB0
-#define sw_uart___rx 	b, 1	// Rx : PORTB1
+#define sw_uart__tx	B, 0	// Tx : PORTB0
+#define sw_uart__rx 	B, 1	// Rx : PORTB1
 
-#define sw_uart___delay_for (2*16/FCY)		//extra delay on 14us write
-#define sw_uart___delay_for_read (2*18/FCY) //extra delay on read 10us
+#define sw_uart__delay_for (2*16/FCY)		//extra delay on 14us write
+#define sw_uart__delay_for_read (2*18/FCY) //extra delay on read 10us
 
-#define sw_uart___bitperiod ((1000000/sw_uart__baudrate))	// duration of 1 bit in microseconds
+#define sw_uart__bitperiod ((1000000/sw_uart__baudrate))	// duration of 1 bit in microseconds
 
-#define sw_uart___bitperiod_for (sw_uart___bitperiod - sw_uart___delay_for)	// actual delay on write
-#define sw_uart___bitperiod_for_read (sw_uart___bitperiod - sw_uart___delay_for_read)	// actual read delay
-#define sw_uart___halfbitperiod (sw_uart___bitperiod/2)		// half bit duration
+#define sw_uart__bitperiod_for (sw_uart__bitperiod - sw_uart__delay_for)	// actual delay on write
+#define sw_uart__bitperiod_for_read (sw_uart__bitperiod - sw_uart__delay_for_read)	// actual read delay
+#define sw_uart__halfbitperiod (sw_uart__bitperiod/2)		// half bit duration
 
 
 __global (
@@ -24,10 +24,10 @@ __global (
 )
 
 fn init() {
-    pin.high(sw_uart__tx);
-    pin.high(sw_uart__rx);
-    pin.setup(sw_uart__tx, pin.output);
-    pin.setup(sw_uart__rx, pin.input);
-    pin.high(sw_uart__tx);
-    time.sleep_us(sw_uart__bitperiod);
+    pin.high(sw_uart.tx);
+    pin.high(sw_uart.rx);
+    pin.setup(sw_uart.tx, pin.output);
+    pin.setup(sw_uart.rx, pin.input);
+    pin.high(sw_uart.tx);
+    time.sleep_us(sw_uart.bitperiod);
 }
