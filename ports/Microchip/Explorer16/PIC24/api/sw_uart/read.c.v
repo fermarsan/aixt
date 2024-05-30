c5c9d4a64aecca5bd20f7b1608b47dce0f079b1e
fermarsan <fmartinezsanta@gmail.com>
2024-05-30 12:49:59 -0500

    update

--- ports/Microchip/Explorer16/PIC24/api/sw_uart/read.c.v
+++ ports/Microchip/Explorer16/PIC24/api/sw_uart/read.c.v
@@ -7,13 +7,13 @@ fn read() u8 {
     mask := u8(0x00)
     data := u8(0x00) 
     for pin.read(sw_uart.rx) == 1 {}	//sino llega el bit de star se queda a esperarlo
-    time.sleep_us(sw_uart.halfbitperiod) 
+    time.sleep_cycles(sw_uart.halfbitperiod)
     for mask=0x01; mask!=0; mask=mask<<1 {
-        time.sleep_us(sw_uart.bitperiod_for_read)    //retardo teniendo en cuenta os del for y el if es de 4 ciclos de reloj
+        time.sleep_cycles(sw_uart.bitperiod) //_for_read)    //retardo teniendo en cuenta os del for y el if es de 4 ciclos de reloj
         if pin.read(sw_uart.rx) {
 			data = data | mask
 		} 
     }
-    time.sleep_us(sw_uart.bitperiod) 
+    time.sleep_cycles(sw_uart.bitperiod)
     return data 
 }
