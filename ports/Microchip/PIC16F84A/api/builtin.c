6422639adf1d8e1e86b75ff4774113daa938b9e6
fermarsan <fmartinezsanta@gmail.com>
2024-09-08 16:57:07 -0500

    PIC16F8x port API update

--- ports/Microchip/PIC16F84A/api/builtin.c
+++
@@ -1,6 +0,0 @@
-#define _XTAL_FREQ 4000000
-
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-#pragma config WDTE = OFF       // Watchdog Timer (WDT disabled)
-#pragma config PWRTE = ON       // Power-up Timer Enable bit (Power-up Timer is disabled)
-#pragma config CP = OFF         // Code Protection bit (Code protection disabled)
