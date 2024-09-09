f8d85b2e71b00a74225265d82273e54bd93d1385
fermarsan <fmartinezsanta@gmail.com>
2024-09-08 21:52:18 -0500

    Arduino Nano 4-bit echo example update

---
+++ ports/Arduino/Arduino-Nano/examples/Nano-port_echo/Nano-port_echo.v
@@ -0,0 +1,18 @@
+// Project Name: 4-bit echo
+// Author: Fernando M. Santa
+// Date: 08/0
+// Arduino-Nano board
+// 4-bit echo, from PORTB LSBs to PORTC LSBs
+
+import time
+import port
+
+port.setup(port.b, ~0b11111111)	// all inputs
+port.setup(port.c, ~0b11110000)	// 4 LSBs outputs
+
+for {
+	port.write(port.c, port.read(port.b))
+	time.sleep_ms(100)
+}
+
+
--- ports/Microchip/PIC16F8x/examples/16F84_port_echo/main.v
+++ ports/Microchip/PIC16F8x/examples/16F84_port_echo/main.v
@@ -2,7 +2,7 @@
 // Author: Fernando M. Santa
 // Date: 05/09/2024
 // Echo from the 4 LSBs of PORTA to the 4 LSBs of PORTB
-// PIC16F84 fosc = 4Mhzz
+// PIC16F84 fosc = 4Mhz
 
 import time
 import port
