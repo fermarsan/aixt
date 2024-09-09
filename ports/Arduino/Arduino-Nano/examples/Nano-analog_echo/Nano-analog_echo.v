8cfc041c308258b0584a27a5a95111dfd201224d
fermarsan <fmartinezsanta@gmail.com>
2024-09-09 00:44:14 -0500

    Arduino Nano breathing example update

--- ports/Arduino/Arduino-Nano/examples/Nano-analog_echo/Nano-analog_echo.v
+++ ports/Arduino/Arduino-Nano/examples/Nano-analog_echo/Nano-analog_echo.v
@@ -8,14 +8,11 @@ import pin
 import adc
 import pwm
 
-pin.setup(pin.d13, pin.output)
+pin.setup(pin.d3, pin.output)
 
 for {
 	value := adc.read(adc.a0)
-	pwm.write(pin.d3 50)
+	pwm.write(pin.d3, u8(value/4))
+	time.sleep_ms(100)
 }
-
-
-pin.high(pin.d13)
-time.sleep_ms(500)
 
