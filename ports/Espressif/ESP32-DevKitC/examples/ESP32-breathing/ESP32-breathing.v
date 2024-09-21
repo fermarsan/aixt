0cd55d098bb90bc8c5a13f42a244b3391b1e6253
fermarsan <fmartinezsanta@gmail.com>
2024-09-21 15:34:29 -0500

    examples updated

--- ports/Espressif/ESP32-DevKitC/examples/ESP32-breathing/ESP32-breathing.v
+++ ports/Espressif/ESP32-DevKitC/examples/ESP32-breathing/ESP32-breathing.v
@@ -1,21 +1,21 @@
 // Project Name: Blinking
 // Author: Fernando M. Santa
-// Date: 08/09/20
-// Arduino-Nano board
+// Date: 21/09/2024
+// ESP32-DevKitC board
 
 import time
 import pin
 import pwm
 
-pin.setup(d3, output)
+pin.setup(io2, output)
 
 for {
 	for level in 0 .. 256 {
-		pwm.write(d3, level)
+		pwm.write(io2, level)
 		time.sleep_ms(5) 
 	}
 	for level in 0 .. 256 {
-		pwm.write(d3, 255-level)
+		pwm.write(io2, 255-level)
 		time.sleep_ms(5) 
 	}
     time.sleep_ms(100)   
