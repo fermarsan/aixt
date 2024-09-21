bfae7bf6e5f261568cd3ea9fc991069e0eaa1733
fermarsan <fmartinezsanta@gmail.com>
2024-09-21 16:38:54 -0500

    API paths print

--- src/aixt_cgen/load_mod_paths.v
+++ src/aixt_cgen/load_mod_paths.v
@@ -9,6 +9,7 @@ import os
 // load_mod_paths function detects all the modules in API folders
 fn (mut gen Gen) load_mod_paths() {
 	api_paths := gen.setup.api_paths
+	// println('${api_paths}')
 	for api_path in api_paths {
 		base_dir := '${gen.transpiler_path}/ports/${api_path}/api'
 		dir_content := os.ls(base_dir) or { [] }
