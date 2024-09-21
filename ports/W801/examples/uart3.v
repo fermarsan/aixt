d8f6bb00e9b31cc0c9db778043cd9a28397645f4
Daniel Vásquez Gómez <davasquezg@udistrital.edu.co>
2024-09-20 20:50:17 -0500

    Se paso a minusculas los archivos de los ejemplos, se transcompilaron todos los ejemplos

---
+++ ports/W801/examples/uart3.v
@@ -0,0 +1,16 @@
+// Project Name: UART ADC
+// Author: Fernando M. Santa
+// Date: 08/09/2024
+// Arduino-Nano board
+
+import time
+import uart
+import adc
+
+uart.setup(9600)
+
+for {
+	analog := adc.read(a1)
+	uart.println('ADC channel 0: ${analog}')
+	time.sleep_ms(1000)
+}
