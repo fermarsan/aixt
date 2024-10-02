7fcd72a74b0270c408309a3a9ff35b712f1475ae
fermarsan <fmartinezsanta@gmail.com>
2024-10-02 17:46:43 -0500

    all versions of `match` expressions (single or multiple cases) implemented and tested

--- src/aixt_cgen/match_branch.v
+++ src/aixt_cgen/match_branch.v
@@ -14,24 +14,57 @@ fn (mut gen Gen) match_branch(node ast.MatchBranch) []string { // statements mat
 	for st in node.stmts {
 		stmts << gen.ast_node(st).join('')
 	}
-	if gen.match_as_nested_if {
-		if node.is_else {
-		out << $tmpl('c_templates/else_block.tmpl.c')#[..-1]
+	if node.is_else {
+		if gen.match_as_nested_if {
+			out << $tmpl('c_templates/else_block.tmpl.c')#[..-1]
 		} else {
-			mut cond := ''
-			for ex in node.exprs {
-				cond += '(${ex.str()}) == ${gen.cur_cond} || '
+			out << $tmpl('c_templates/match_else_branch.tmpl.c')#[..-1]
+		}
+	} else {
+		mut cond := ''
+		match node.exprs[0] {
+			ast.InfixExpr {	// comparisons: a < 5
+				for ex in node.exprs {
+					exp := gen.ast_node(ex).join('')
+					cond += '(${exp}) == ${gen.cur_cond} || '
+				} 
+				cond = cond#[..-4]	// remove the last ' ||
+				out << $tmpl('c_templates/if_block.tmpl.c')#[..-1]
 			}
-			cond = cond#[..-4]
-			out << $tmpl('c_templates/if_block.tmpl.c')#[..-1]
+			ast.RangeExpr {	// ranges: 0 ... 9				
+				for ex in node.exprs {
+					exp := (ex as ast.RangeExpr)
+					cond += '(${gen.cur_cond} >= ${gen.ast_node(exp.low).join('')} && ' +
+							'${gen.cur_cond} <= ${gen.ast_node(exp.high).join('')}) || '
+				} 
+				cond = cond#[..-4]	// remove the last '
+				out << $tmpl('c_templates/if_block.tmpl.c')#[..-1]
+			}
+			else {	// literals or identifiers: 7
+				exprs := node.exprs.map(gen.ast_node(it).join(''))	
+				out << $tmpl('c_templates/match_branch.tmpl.c')#[..-1]
+			}
 		}
-	} else { // regular match expressions
-		if node.is_else {
-		out << $tmpl('c_templates/match_else_branch.tmpl.c')#[..-1]
-		} else {
-			exprs := node.exprs.map(it.str())
-			out << $tmpl('c_templates/match_branch.tmpl.c')#[..-1]
-		}
+
+	// if gen.match_as_nested_if {
+	// 	if node.is_else {
+	// 	out << $tmpl('c_templates/else_block.tmpl.c')#[..-1]
+	// 	} else {
+	// 		mut cond := ''
+	// 		for ex in node.exprs {
+	// 			cond += '(${ex.str()}) == ${gen.cur_cond} || '
+	// 		}
+	// 		cond = cond#[..-4]
+	// 		out << $tmpl('c_templates/if_block.tmpl.c')#[..-1]
+	// 	}
+	// } else { // regular match expressions
+	// 	if node.is_else {
+	// 	out << $tmpl('c_templates/match_else_branch.tmpl.c')#[..-1]
+	// 	} else {
+	// 		exprs := node.exprs.map(it.str())
+	// 		out << $tmpl('c_templates/match_branch.tmpl.c')#[..-1]
+	// 	}
+	// }
 	}
 	return out
 }
