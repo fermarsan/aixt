7b13c0c622ca4baf4cf2d68e007887015138b3be
fermarsan <fmartinezsanta@gmail.com>
2024-11-15 18:19:03 -0500

    Arduino new API update (UART)

--- ports/Arduino/arduino-generic-UART/api/uart/uart.c.v
+++ ports/Arduino/arduino-generic-UART/api/uart/uart.c.v
@@ -6,4 +6,9 @@
 // Description: UART module (Arduino Nano - ATmega328P port)
 module uart
 
-
+fn C.Serial.begin(baud_rate int)
+fn C.Serial.write(character u8) 
+fn C.Serial.read() u8
+fn C.Serial.print(message string)
+fn C.Serial.println(message string)
+fn C.Serial.available() int
