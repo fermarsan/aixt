a0e2d6f95a610ed4a37868cda634c836f0860d61
fermarsan <fmartinezsanta@gmail.com>
2024-12-03 17:50:42 -0500

    rename arduino-generic-low-mem to arduino-AVR.

--- ports/Arduino/arduino-generic-low-mem/api/pwm/pwm.c.v
+++
@@ -1,9 +0,0 @@
-// Project Name: Aixt, https://github.com/fermarsan/aixt.git
-// Author: Fernando M. Santa
-// Date: 2024
-// License: MIT
-//
-// Description: PWM functions (Arduino Nano - ATmega328P port)
-module pwm
-
-fn C.analogWrite(name u8, value u8)
--- ports/Arduino/arduino-generic-low-mem/api/pwm/v.mod
+++
@@ -1,7 +0,0 @@
-Module {
-	name:	'pwm'
-	description:	'Aixt PWM module for Arduino devices'
-	version:	'0.1.2'
-	license:	'MIT'
-	dependencies:	[]
-}
