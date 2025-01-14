c4448cc7ea8ebbdaba0354701d51d0d686d857bf
fermarsan <fmartinezsanta@gmail.com>
2025-01-14 06:16:03 -0500

    Blue-Pill and Nucleo-L031K6 updated

--- ports/ST/Blue-Pill/api/uart/uart.c
+++
@@ -1,7 +0,0 @@
-#define SERIAL_BEGIN(BAUD_RATE)    Serial.begin(BAUD_RATE)
-#define SERIAL_WRITE(CHARACTER)    Serial.write(CHARACTER) 
-#define SERIAL_READ()              Serial.read()
-#define SERIAL_PRINT(MESSAGE)      Serial.print(MESSAGE)
-#define SERIAL_PRINTLN(MESSAGE)    Serial.println(MESSAGE)
-#define SERIAL_AVAILABLE()         Serial.available()    
-// #define SERIAL_PINS(tx, rx)        UART Serial(tx, rx, 0, 0)
