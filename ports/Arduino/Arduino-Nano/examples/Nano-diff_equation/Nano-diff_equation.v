76412117ba91f6a7fed2b27d5eccc53aeb8f4b6f
fermarsan <fmartinezsanta@gmail.com>
2025-01-18 11:05:26 -0500

    New Nano example

---
+++ ports/Arduino/Arduino-Nano/examples/Nano-diff_equation/Nano-diff_equation.v
@@ -0,0 +1,37 @@
+// Project name: Ecuación en Diferencias RC
+// Author: Yo
+// Date: 17/01/2025
+// Arduino Nano board
+
+import time
+import pin
+import port
+import adc
+
+pin.setup(adc.ch0, pin.input)	// entrada
+port.setup(port.d, port.all_outputs)	// salidas
+
+pin.setup(pin.d8, pin.output)	// salida de referencia par el Ts
+
+mut vck := 0 	// variables para los estados
+mut vfk := 0	// actuales y anteriores
+mut vck_1 := 0	// de la entrada y la salida
+mut vfk_1 := 0
+
+for {
+    // paso 1: rotar los vectores de datos
+    vfk_1 = vfk
+    vck_1 = vck
+    // paso 2: tomar una muestra nueva de la entrada
+    vfk = adc.read(adc.ch0) >> 2   
+    // paso 3: calcular el nuevo valor de la salida 
+    //      (ecuación en diferencias)
+    vck = int(f32(vck_1)*0.8187 + f32(vfk_1)*0.1813)
+    // paso 4: actualizar la salida
+    port.write(port.d, u8(vck))
+	// paso 5: garantizar el Ts
+	pin.high(pin.d8)
+	pin.low(pin.d8)
+	time.sleep_ms(19)
+	time.sleep_us(866)
+}
