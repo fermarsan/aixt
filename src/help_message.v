d8091a2b41de102a150639fcfb87146c8b8f3e66
fermarsan <fmartinezsanta@gmail.com>
2025-01-21 21:13:12 -0500

    Task done: Help message updated

--- src/help_message.v
+++ src/help_message.v
@@ -21,13 +21,13 @@ Usage:
   aixt [command] [device] [input_file_name]
 
 Examples:
-  aixt transpile NXC hello.v	     Transpile the file `hello.v`  and output it as
-	                                 `hello.nxc` .
+  aixt transpile NXC hello.v       Transpile the file `hello.v`  and output it as
+                                   `hello.nxc` .
   aixt build NXT hello.v           Transpile, compile and download `hello.v`  to NXT
                                    Intelligent brick.
-  aixt -t Arduino_Nano hello.v	   Transpile the file `hello.v`  and output it as
+  aixt -t Arduino_Nano hello.v     Transpile the file `hello.v`  and output it as
                                    `hello.c` or `hello.ino`  (depending on the Backend).
-  aixt -c Emulator hello.v	       Compile a previously transpiled `hello.c`  (from
+  aixt -c Emulator hello.v         Compile a previously transpiled `hello.c`  (from
                                    `hello.v`) and output it as `hello` or `hello.exe`
   aixt clean NXC hello.v           Clean all the generated files `hello.*`
   aixt -h                          Print this help message
@@ -37,13 +37,13 @@ Examples:
   aixt -np NXT /home proj_1        Create the `proj_1` NXT project in `/home`
 
 aixt supports the following commands:
-  transpile, -t                 Transpile to C an Aixt source file.
-  compile, -c                   Compile the previosly generated C file.
-  build, -b                     Build (transpile and compile) an Aixt program.
-  flash, -f                     Flash the device with the generated binary file 
-  clean, -cl                    Clean all the generated file (C and executables).
-  help, --help, -h              Call this help
-  version, --version, -v        Returns the Aixt version number
-  new_project, -np              Create new project (main.v and editor setup files)
-  symlink, -s                   Create a symbolic link to the Aixt source file.'
+  transpile, -t                    Transpile to C an Aixt source file.
+  compile, -c                      Compile the previosly generated C file.
+  build, -b                        Build (transpile and compile) an Aixt program.
+  flash, -f                        Flash the device with the generated binary file 
+  clean, -cl                       Clean all the generated file (C and executables).
+  help, --help, -h                 Call this help
+  version, --version, -v           Returns the Aixt version number
+  new_project, -np                 Create new project (main.v and editor setup files)
+  symlink, -s                      Create a symbolic link to the Aixt source file.'
 }
