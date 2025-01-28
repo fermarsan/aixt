88e14803303e777d9447dff82b104ea12eb2964b
fermarsan <fmartinezsanta@gmail.com>
2025-01-28 09:33:17 -0500

    update

--- ports/LILYGO/T-WATCH-2020/api/time/time.c.v
+++
@@ -1,8 +0,0 @@
-// Project name: Aixt, https://github.com/fermarsan/aixt.git
-// Author: Fernando M. Santa
-// Date: 2024
-// License: MIT
-//
-// Description: Time module (Arduino devices)
-module time
-
