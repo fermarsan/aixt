5fe3100927266e6539a6b7082eb81cab80c2bf8a
Zanmy05 <lngonzalezh@udistrital.edu.co>
2025-02-08 22:32:05 -0500

    Segunda Actualización Libreria OLED

--- lib/oled/arduino/bitmap.c.v
+++ lib/oled/arduino/bitmap.c.v
@@ -5,7 +5,3 @@ pub fn drawbitmap(x any , y any , array_image any , width any , height any , col
 	C.DISPLAY_DRAWBITMAP(x,y,array_image,width,height,color)
 	C.DISPLAY_DISPLAY()
 }
-//display.drawBitmap(95, 35, circleArrows1, 32, 29, WHITE);
-
-//void drawXBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w,
-                   //int16_t h, uint16_t color);
--- lib/oled/arduino/oled.c.v
+++ lib/oled/arduino/oled.c.v
@@ -7,23 +7,18 @@ module oled
 @[as_macro] pub const inverse   = 2
 
 
-fn C.DISPLAY_BEGIN (type_ u8, address int) bool
-fn C.DISPLAY_CLEARDISPLAY() u8
-fn C.DISPLAY_SETTEXTSIZE(number int)
-fn C.DISPLAY_TEXTCOLOR(character u8)
-fn C.DISPLAY_SETCURSOR(x,y u8)
-fn C.DISPLAY_PRINTLN(message string)
-fn C.DISPLAY_DISPLAY() u8
-fn C.DISPLAY_DRAWPIXEL(x,y,color u8, int)
-fn C.DISPLAY_DRAWRECT(x any, y any , width any, height,color u8, int)
-fn C.DISPLAY_FILLRECT(x,y,width,height,color u8, int)
-fn C.DISPLAY_DRAWCIRCLE(x,y,radio,color u8, int)
-fn C.DISPLAY_FILLCIRCLE(x,y,radio,color u8, int) 
-fn C.DISPLAY_DRAWBITMAP(x any,y any,array_image any,width any,height any,color any) 
+fn C.DISPLAY_BEGIN (type_ any , address any) bool
+fn C.DISPLAY_CLEARDISPLAY(any) 
+fn C.DISPLAY_SETTEXTSIZE(number any)
+fn C.DISPLAY_TEXTCOLOR(character any)
+fn C.DISPLAY_SETCURSOR(x any , y any)
+fn C.DISPLAY_PRINTLN(message any)
+fn C.DISPLAY_DISPLAY(any) 
+fn C.DISPLAY_DRAWPIXEL(x any , y any , color any)
+fn C.DISPLAY_DRAWRECT(x any , y any , width any , height any , color any)
+fn C.DISPLAY_FILLRECT(x any , y any , width any , height any , color any)
+fn C.DISPLAY_DRAWCIRCLE(x any , y any , radio any , color any)
+fn C.DISPLAY_FILLCIRCLE(x any , y any , radio any , color any) 
+fn C.DISPLAY_DRAWBITMAP(x any , y any , array_image any , width any , height any , color any) 
 
-// cada variable lleva el tipo
-// colocar any a todos, si no revisar tipo de datos 
-// definir constantes propias 
-//#define SSD1306_BLACK 0   ///< Draw 'off' pixels
-//#define SSD1306_WHITE 1   ///< Draw 'on' pixels
-//#define SSD1306_INVERSE 2 ///< Invert pixels
+
