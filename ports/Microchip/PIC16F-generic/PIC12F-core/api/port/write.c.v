8720782b48ac3c3027c9bd438fa99a16311f4ba8
fermarsan <fmartinezsanta@gmail.com>
2025-02-12 00:09:47 -0500

    New PIC16F family organization

---
+++ ports/Microchip/PIC16F-generic/PIC12F-core/api/port/write.c.v
@@ -0,0 +1,15 @@
+// Project name: Aixt project, https://github.com/fermarsan/aixt.git
+// Author: Fernando M. Santa
+// Date: 2024
+// License: MIT
+//
+// Description: Pin-port functions (PIC16F8x port)
+module port
+
+// write function writes an 8 bit value on a port
+@[as_macro]
+pub fn write(name u8, value u8) { 
+	unsafe {
+		*(&(C.PORTA) + name) = value
+	}	
+}
