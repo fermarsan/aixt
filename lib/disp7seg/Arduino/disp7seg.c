8b7f6d8542e39849bb6b9598addcdb8095c1f1c0
Ozkamb107 <os-camb@hotmail.com>
2025-02-28 09:33:25 -0500

    nuevo1

--- lib/disp7seg/Arduino/disp7seg.c
+++ lib/disp7seg/Arduino/disp7seg.c
@@ -1,9 +1,9 @@
 #define DIGITSHIELD_BEGIN()                                         DigitShield.begin()
-#define DIGITSHIELD_SETVALUE(P1)                                    DigitShield.setValue(P1) 
-#define DIGITSHIELD_SETPRECISION(P1)                                DigitShield.setPrecision(P1)
-#define DIGITSHIELD_SETLEADINGZEROS(P1)                             DigitShield.setLeadingZeros(P1)
-#define DIGITSHIELD_SETBLANK(P1)                                    DigitShield.setBlank(P1)
-#define DIGITSHIELD_SETDIGIT(P1,P2)                                 DigitShield.setDigit(P1,P2)    
-#define DIGITSHIELD_SETDECIMALPOINT(P1,P2)                          DigitShield.setDecimalPoint(P1,P2)  
+#define DIGITSHIELD_SETVALUE(p1)                                    DigitShield.setValue(p1) 
+#define DIGITSHIELD_SETPRECISION(p1)                                DigitShield.setPrecision(p1)
+#define DIGITSHIELD_SETLEADINGZEROS(p1)                             DigitShield.setLeadingZeros(p1)
+#define DIGITSHIELD_SETBLANK(p1)                                    DigitShield.setBlank(p1)
+#define DIGITSHIELD_SETDIGIT(p1,p2)                                 DigitShield.setDigit(p1,p2)    
+#define DIGITSHIELD_SETDECIMALPOINT(p1,p2)                          DigitShield.setDecimalPoint(p1,p2)  
 
 
