9063ee51f0f91f69482d402b2d440ef2c7514186
fermarsan <fmartinezsanta@gmail.com>
2025-03-03 19:28:39 -0500

    New port Luatos/ESP32-C3-CORE

--- TODO.md
+++ TODO.md
@@ -64,3 +64,5 @@ Tasks to do in Aixt
 - [x] Change the External Interrupt for Arduino Nano using C maros with string concatenation
 - [x] Add an external interrupt example for Arduino Nano
 - [x] Unify the API of Arduino supported devices 
+- [x] Update the ESP32-C3FH4 port
+- [x] New port Luatos/ESP32-C3-CORE
---
+++ ports/Luatos/ESP32-C3-CORE/api/pwm/pwm.c.v
@@ -0,0 +1,25 @@
+// Project Name : Aixt: http://github.com/fermansan/aixt.git
+// Author: Fernando M. Santa 
+// License : MIT
+// Date: 2025
+
+module pwm 
+
+
+pub const ch0 = 0 
+pub const ch1 = 1
+pub const ch2 = 2
+pub const ch3 = 3
+pub const ch4 = 4
+pub const ch5 = 5
+pub const ch6 = 6
+pub const ch7 = 7
+pub const ch8 = 8
+pub const ch9 = 9
+pub const ch10 = 10
+pub const ch11 = 11
+pub const ch12 = 12
+pub const ch13 = 13
+
+pub const ch18 = 18
+pub const ch19 = 19
