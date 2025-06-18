7cbe07cd009fa432d6ebbf84515615e5e1bb0f4b
fermarsan <fmartinezsanta@gmail.com>
2025-06-17 20:45:16 -0500

    update

--- ports/Arduino/Arduino-Nano/examples/Nano-external_interrupt/Nano-external_interrupt.v
+++ ports/Arduino/Arduino-Nano/examples/Nano-external_interrupt/Nano-external_interrupt.v
@@ -6,22 +6,16 @@
 import pin
 import ext
 
-// __global (
-// 	state = false
-// )
-
 @[ext_isr:'pin.d2']
 fn blink() {
-	// state = !state
 	pin.toggle(led_0)
 }
 
 pin.setup(pin.d2, pin.input)
-// pin.setup(pin.d13, pin.output)
 pin.low(led_0)
 
 ext.irq_enable(pin.d2, ext.change)
 
 for {
-	// pin.write(pin.d13, u8(state))
+	
 }
