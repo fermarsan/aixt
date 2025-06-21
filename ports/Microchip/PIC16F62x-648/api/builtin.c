1487ace350415e9ef1d79b6ca70b07e97d99c124
fermarsan <fmartinezsanta@gmail.com>
2025-06-21 18:41:41 -0500

    PIC16F627 example

--- ports/Microchip/PIC16F62x-648/api/builtin.c
+++ ports/Microchip/PIC16F62x-648/api/builtin.c
@@ -2,7 +2,7 @@
 #define _XTAL_FREQ _const_main__cpu_freq
 
 #pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-#pragma config BOR = ON         // Brown-out Reset enabled
+#pragma config BOREN = ON       // Brown-out Reset enabled
 #pragma config WDTE = OFF       // Watchdog Timer (WDT disabled)
 #pragma config PWRTE = ON       // Power-up Timer Enable bit (Power-up Timer is disabled)
 #pragma config CP = OFF         // Code Protection bit (Code protection disabled)
