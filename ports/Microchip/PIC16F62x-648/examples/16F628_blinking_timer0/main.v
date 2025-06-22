0506cd5a13f0b0e787708c6979ba9b40a884c000
fermarsan <fmartinezsanta@gmail.com>
2025-06-21 19:11:24 -0500

    PIC16F628 example

--- ports/Microchip/PIC16F62x-648/examples/16F628_blinking_timer0/main.v
+++ ports/Microchip/PIC16F62x-648/examples/16F628_blinking_timer0/main.v
@@ -1,22 +1,19 @@
 // Project name: Blinking
 // Author: Fernando M. Santa
 // Date: 2024-2025
-// PIC16F83 fosc = 4Mhz by default
+// PIC16F628 fosc = 8Mhz
 
 import pin 
 import timer0
 
 
+@[as_macro] const cpu_freq = 8_000_000	// sets fosc to 8 Mhz
 
 @[timer0_isr]	// interrupt service routine for timer0 overload
 fn blinking() {
 	timer0.restart()
 	pin.toggle(pin.b4)
 }
-
-// @[as_macro] const cpu_freq = 8_000_000	// sets fosc to 8 Mhz
-// @[as_macro] const cpu_freq = 12_000_000	// sets fosc to 12 Mhz
-// @[as_macro] const cpu_freq = 20_000_000	// sets fosc to 20 Mhz
 
 pin.setup(pin.b4, pin.output)
 timer0.setup(10_000)	// configure the timer0 with a period of 10ms (10000us)
