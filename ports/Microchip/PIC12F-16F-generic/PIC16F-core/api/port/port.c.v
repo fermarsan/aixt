023d0770177be2917146ac9bfdcd84e16c6aa700
fermarsan <fmartinezsanta@gmail.com>
2025-07-01 18:00:17 -0500

    update port module for PIC16F

--- ports/Microchip/PIC12F-16F-generic/PIC16F-core/api/port/port.c.v
+++ ports/Microchip/PIC12F-16F-generic/PIC16F-core/api/port/port.c.v
@@ -6,20 +6,13 @@
 // Description: Pin-port functions for 16F family
 module port
 
-// port names
-@[as_macro]
-pub const a = 0
-@[as_macro]
-pub const b = 1
-@[as_macro]
-pub const c = 2
-@[as_macro]
-pub const d = 3
-@[as_macro]
-pub const e = 4
+// // port names
+// @[as_macro] pub const a = 0
+// @[as_macro] pub const b = 1
+// @[as_macro] pub const c = 2
+// @[as_macro] pub const d = 3
+// @[as_macro] pub const e = 4
 
-// port common modes
-@[as_macro]
-pub const all_inputs = 0b11111111
-@[as_macro]
-pub const all_outputs = 0b00000000
+// // port common modes
+// @[as_macro] pub const all_inputs = 0b11111111
+// @[as_macro] pub const all_outputs = 0b00000000
