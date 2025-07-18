0ff963307d6ee782178c83d2f2fc03efb8102a6d
fermarsan <fmartinezsanta@gmail.com>
2025-07-18 18:35:27 -0500

    update adc PSOC 4

--- ports/Cypress/CY8CKIT-145-40XX/api/adc/read.c.v
+++ ports/Cypress/CY8CKIT-145-40XX/api/adc/read.c.v
@@ -2,8 +2,10 @@
 // Author: Fernando M. Santa
 // Date: 2024
 // License: MIT
-//
-// Description: ADC functions
 module adc
 
-fn C.analogRead(name u8) u16
+//read function reads the value from the specified analog pin
+@[as_macro]
+pub fn read(name u8) u16 {
+	return C.analogRead(name)
+}
