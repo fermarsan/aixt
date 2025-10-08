71bed195ac84d2896de67c4fd1bb43e4cd8dc442
fermarsan <fmartinezsanta@gmail.com>
2025-10-08 00:17:27 -0500

    Update the Explorer16 target

--- targets/Microchip/PIC24F-dsPIC33-generic/api/pin/pin.c.v
+++ targets/Microchip/PIC24F-dsPIC33-generic/api/pin/pin.c.v
@@ -6,6 +6,18 @@
 // Description: Pin management functions PIC24 Family
 module pin
 
+// setup configures the mode of a pin
+@[as_macro]
+pub fn setup(id u8, mode u8) {
+	unsafe { 
+		if mode == 1 { // as input (1)
+			*(&C.TRISA + u16(id / 16)*3)  |=  (0x0001 << (id % 16))
+		} else {  // as output (0)    
+			*(&C.TRISA + u16(id / 16)*3)  &=  ~(0x0001 << (id % 16))
+		}	
+	}
+}
+
 // high puts a logic 1 to a pin
 @[as_macro]
 pub fn high(id u8) {
@@ -22,23 +34,12 @@ pub fn low(id u8) {
 	}
 }
 
-// read function reads the logic value of a pin
+// write function writes a logic value to a pin
 @[as_macro]
-pub fn read(id u8) u8 {
+pub fn write(id u8, value u8) {
 	unsafe {
-		return	u8( (*(&C.PORTA + u16(id / 16)*3) >> (id % 16))  &  0x0001 )
-	}
-}
-
-// setup configures the mode of a pin
-@[as_macro]
-pub fn setup(id u8, mode u8) {
-	unsafe { 
-		if mode == 1 { // as input (1)
-			*(&C.TRISA + u16(id / 16)*3)  |=  (0x0001 << (id % 16))
-		} else {  // as output (0)    
-			*(&C.TRISA + u16(id / 16)*3)  &=  ~(0x0001 << (id % 16))
-		}	
+		*(&C.LATA + u16(id / 16)*3)  &=  (~(0x0001 << (id % 16)))
+		*(&C.LATA + u16(id / 16)*3)  |=  (value << (id % 16))
 	}
 }
 
@@ -50,11 +51,10 @@ pub fn toggle(id u8) {
 	}
 }
 
-// write function writes a logic value to a pin
+// read function reads the logic value of a pin
 @[as_macro]
-pub fn write(id u8, value u8) {
+pub fn read(id u8) u8 {
 	unsafe {
-		*(&C.LATA + u16(id / 16)*3)  &=  (~(0x0001 << (id % 16)))
-		*(&C.LATA + u16(id / 16)*3)  |=  (value << (id % 16))
+		return	u8( (*(&C.PORTA + u16(id / 16)*3) >> (id % 16))  &  0x0001 )
 	}
 }
