a512b73697df4337be8036ea064f4ce2834c6b82
fermarsan <fmartinezsanta@gmail.com>
2025-12-28 14:55:58 -0500

    start generation the automatic documentation for PIC16F targets

---
+++ docs/Microchip/PIC16/PIC16F_generic/pin_fn_html/search_index.js
@@ -0,0 +1,26 @@
+var searchModuleIndex = [
+"pin_fn",
+];
+
+var searchIndex = [
+"setup",
+"high",
+"low",
+"write",
+"toggle",
+"read",
+];
+
+var searchModuleData = [
+["_File:_ https://github.com/fermarsan/aixt/blob/main/targets/Microchip/PIC16F_gen","pin_fn.html"],
+];
+
+var searchData = [
+["pin_fn","setup function configures the mode of a pin. `id` is the pin id for example `pin","pin_fn.html#setup","fn "],
+["pin_fn","high function puts a logic `1` to a pin through its `id`","pin_fn.html#high","fn "],
+["pin_fn","low function puts a logic `0` to a pin through its `id`","pin_fn.html#low","fn "],
+["pin_fn","write function writes a logic `value` to a pin through its `id`","pin_fn.html#write","fn "],
+["pin_fn","toggle function toggles the logic `value` of a pin through its `id`","pin_fn.html#toggle","fn "],
+["pin_fn","read function reads the logic value of a pin pin through its `id`","pin_fn.html#read","fn "],
+];
+
