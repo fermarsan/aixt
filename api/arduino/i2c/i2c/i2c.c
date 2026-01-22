ba414bb07e40c1a78c79b0aaafe3e39390695865
fermarsan <fmartinezsanta@gmail.com>
2026-01-21 20:22:40 -0500

    spi & i2c update

--- api/arduino/i2c/i2c/i2c.c
+++ api/arduino/i2c/i2c/i2c.c
@@ -1,9 +1,3 @@
-// Authors:
-//        - Juan Pablo Bernal
-//        - Daniela Mendoza Deantonio
-//        - Fernando M. Santa
-// Date: 2024
-
 #define WIRE_BEGIN(...)                         Wire.begin(__VA_ARGS__)
 #define WIRE_END()                              Wire.end()
 #define WIRE_REQUESTFROM(ADDRESS,QUANTITY)      Wire.requestFrom(ADDRESS,QUANTITY)
