71ce33a4ea0a5825cafb05012c6e6cff7c339ebc
fermarsan <fmartinezsanta@gmail.com>
2026-09-12 14:24:45 -0500

    start modifying `c_compile_cmd` to load setup files from the project's folder as well as the Aixt folder

--- src/aixt/setup/load.v
+++ src/aixt/setup/load.v
@@ -8,7 +8,7 @@ module setup
 import os
 import json
 
-// load function loads the device and the compiler setup files (from aixt path)
+// load function loads the device and the compiler setup files
 pub fn (mut stp Setup) load(device string, proj_path string) {
 
 	aixt_path := os.dir(os.dir(os.executable()))
@@ -26,16 +26,24 @@ pub fn (mut stp Setup) load(device string, proj_path string) {
 		device
 	}
 
-	dev_setup := os.read_file(os.norm_path('${aixt_path}/setup/${target}.json')) or { panic(err) }
+	// load the device setup file
+	dev_setup := if os.exists(os.norm_path('${proj_path}/.setup/${target}.json')) {	// from project's folder
+					os.read_file(os.norm_path('${proj_path}/.setup/${target}.json')) or { panic(err) }
+				 } else {															// from Aixt's folder
+					os.read_file(os.norm_path('${aixt_path}/setup/${target}.json')) or { panic(err) }
+			 	 }
 	setup := json.decode(Setup, dev_setup) or { panic(err) }
 
-	comp_setup := os.read_file(os.norm_path('${aixt_path}/${setup.compiler_setup_path}')) or { panic(err) }
+	// load the compiler setup file
+	comp_setup := if os.exists(os.norm_path('${proj_path}/.${setup.compiler_setup_path}')) { // from project's folder
+					os.read_file(os.norm_path('${proj_path}/.${setup.compiler_setup_path}')) or { panic(err) }
+				  } else {																	 // from Aixt's folder
+					os.read_file(os.norm_path('${aixt_path}/${setup.compiler_setup_path}')) or { panic(err) }
+			 	  }
 	complete_setup :=  dev_setup.all_before_last('}') + ',\n' + comp_setup.all_after_first('{')
-	stp = json.decode(Setup, complete_setup) or { panic(err) }
-	
 
-	if os.exists(os.norm_path('${path}/.setup/${target}.json')) $$
-	   os.exists(os.norm_path('${path}/.setup/${target}.json')) $$	// inside
+	// load the complete setup data
+	stp = json.decode(Setup, complete_setup) or { panic(err) }
 
 
 	// ---------------- Normalize paths --------------------
@@ -53,41 +61,3 @@ pub fn (mut stp Setup) load(device string, proj_path string) {
 	println(os.norm_path('\t${aixt_path}/${stp.compiler_setup_path}\n'))
 }
 
-
-// load_from_project function loads the device and the compiler setup files (from the project's path)
-pub fn (mut stp Setup) load_from_project(device string, proj_path string) {
-
-	aixt_path := os.dir(os.dir(os.executable()))
-
-	aliases := json.decode(
-		map[string]string,
-		os.read_file(os.norm_path('${aixt_path}/setup/aliases.json')) or { panic(err) }
-	) or { panic(err) }
-	
-	target := if device in aliases {
-		aliases[device]
-	} else {
-		device
-	}
-
-	dev_setup := os.read_file(os.norm_path('${proj_path}/.setup/${target}.json')) or { panic(err) }
-	setup := json.decode(Setup, dev_setup) or { panic(err) }
-
-	comp_setup := os.read_file(os.norm_path('${proj_path}/.${setup.compiler_setup_path}')) or { panic(err) }
-	complete_setup :=  dev_setup.all_before_last('}') + ',\n' + comp_setup.all_after_first('{')
-	stp = json.decode(Setup, complete_setup) or { panic(err) }
-	
-	// ---------------- Normalize paths --------------------
-	stp.device = device
-	stp.cc['path'] = os.norm_path(stp.cc['path'])
-	stp.flasher['path'] = os.norm_path(stp.flasher['path'])
-	mut temp_paths := []string{}
-	for path in stp.api_paths {
-		temp_paths << os.norm_path(path)
-	}
-	stp.api_paths = temp_paths
-	stp.compiler_setup_path = os.norm_path(stp.compiler_setup_path)
-
-	println('Setup files:\n\t' + os.norm_path('${aixt_path}/setup/${stp.target}.json'))
-	println(os.norm_path('\t${aixt_path}/${stp.compiler_setup_path}\n'))
-}
--- src/c_compile_cmd.v
+++ src/c_compile_cmd.v
@@ -26,7 +26,7 @@ fn c_compile_cmd(cmd cli.Command) ! {
 		panic('A target name has to be specified as a flag or inside the `v.mod` file.')
 	}
 	mut project_setup := setup.Setup{}
-	project_setup.load(target)
+	project_setup.load(target, path)
 
 	cc := if cmd.flags.get_string('c_compiler')! != '' {	// as a flag
 		cmd.flags.get_string('c_compiler')!
